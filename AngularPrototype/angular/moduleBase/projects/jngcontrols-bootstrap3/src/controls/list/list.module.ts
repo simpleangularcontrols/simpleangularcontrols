2964a8335c378af9bd40bea735d1bbf01ca54600
Philipp Jenni <philipp.jenni@jnetwork.ch>
2019-11-23 09:45:59 +0100

    FIX: Rename JNetworkCommonListboxOptionModule

--- AngularPrototype/angular/moduleBase/projects/jngcontrols-bootstrap3/src/controls/list/list.module.ts
+++ AngularPrototype/angular/moduleBase/projects/jngcontrols-bootstrap3/src/controls/list/list.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgDropdown, NgDropdownOption } from "./dropdown";
 import { NgListbox } from "./listbox";
-import { jNetworkCommonListboxOptionModule } from "@jnetwork/jngcontrols-common";
+import { JNetworkCommonListboxOptionModule } from "@jnetwork/jngcontrols-common";
 import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [NgDropdown, NgDropdownOption, NgListbox],
   imports: [
-    BrowserModule, jNetworkCommonListboxOptionModule, FormsModule
+    BrowserModule, JNetworkCommonListboxOptionModule, FormsModule
   ],
   exports: [NgDropdown, NgDropdownOption, NgListbox]
 })

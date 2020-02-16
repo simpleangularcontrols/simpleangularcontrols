ec63f3861d7cac60c75aec35818210d8a702acd0
Philipp Jenni <philipp.jenni@jnetwork.ch>
2020-02-16 08:47:25 +0100

    Disable Ivy in Modul Fix Bootstrap 4 Build

--- AngularPrototype/angular/moduleBase/projects/jngcontrols-bootstrap4/src/public_api.ts
+++ AngularPrototype/angular/moduleBase/projects/jngcontrols-bootstrap4/src/public_api.ts
@@ -3,48 +3,19 @@
  */
 
 export * from './controls/buttons/button.module';
-export { NgButtonComponent } from './controls/buttons';
-
-export * from './controls/dialog/dialog.module';
-export { NgDialogComponent } from './controls/dialog';
-
 export * from './controls/checkbox/checkbox.module';
-export { NgCheckboxComponent, NgRadiobuttonComponent, NgRadiobuttonsComponent } from './controls/checkbox';
-
-export * from './controls/list/list.module';
-export { NgDropdownComponent, NgDropdownOptionDirective, NgListboxComponent } from './controls/list';
-
-export * from './controls/validation/validationsummary.module';
-export { NgValidationSummaryComponent } from './controls/validation';
-
-export * from './controls/tabs/tabs.module';
-export { NgTabComponent, NgTabItemComponent } from './controls/tabs';
-
-export * from './controls/input/input.module';
-export { NgInputAreaComponent, NgInputComponent, NgInputCurrencyComponent, NgInputDecimalComponent, NgInputEmailComponent, NgInputIntegerComponent, NgInputPasswordComponent, NgInputSearchComponent } from './controls/input';
-
-export * from './controls/datetime/datetime.module';
-export { NgDateComponent, NgDateSelectorComponent, NgDateTimeComponent, NgTimeComponent } from './controls/datetime';
-
-export * from './controls/tinymce/tinymce.module';
-export { NgTinyMceComponent } from './controls/tinymce';
-
-export * from './controls/static/staticlabel.module';
-export { NgStaticLabelComponent, NgStaticFormContainerComponent } from './controls/static';
-
-export * from './controls/wizard/wizard.module';
-export { NgWizardComponent, NgWizardItemComponent } from './controls/wizard';
-
 export { JNetworkBootstrap4ConfirmModule, ServiceConfirm } from './controls/confirm/confirm.module';
-export { NgConfirmComponent } from './controls/confirm';
-
-export * from './controls/form/form.module';
+export * from './controls/datetime/datetime.module';
+export * from './controls/dialog/dialog.module';
 export * from './controls/form/form';
-export { NgFormularDirective, ProvideParentNgFormularDirective, NGFORM_FACTORY, NGFORMULAR_FACTORY } from './controls/form';
-
+export * from './controls/form/form.module';
 export * from './controls/grid/grid.module';
-export { NgGridComponent, NgPagingComponent, NgGridImageComponent, NgGridButtonComponent, NgGridColumnActionComponent, NgGridColumnComponent } from './controls/grid';
-
+export * from './controls/input/input.module';
+export * from './controls/list/list.module';
 export * from './controls/multilanguage/multilanguage.module';
-export { NgMultilanguageInputComponent, NgMultilanguageInputAreaComponent } from './controls/multilanguage';
+export * from './controls/static/staticlabel.module';
+export * from './controls/tabs/tabs.module';
+export * from './controls/tinymce/tinymce.module';
+export * from './controls/validation/validationsummary.module';
+export * from './controls/wizard/wizard.module';
 

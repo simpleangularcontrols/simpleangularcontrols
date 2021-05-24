5bfda6bd4f71898df976360de2bf8230d5605730
philipp.jenni <philipp.jenni@jnetwork.ch>
2021-05-24 12:34:50 +0200

    feat: Demo für div. Komponenten erstellt fix: Icon bei Time Component korrigiert

--- ch.jnetwork.jng-controls/projects/demo-bootstrap4/src/app/app-routing.module.ts
+++ ch.jnetwork.jng-controls/projects/demo-bootstrap4/src/app/app-routing.module.ts
@@ -8,9 +8,27 @@ const routes: Routes = [
       import('./input/input.module').then((m) => m.InputModule),
   },
   {
+    path: 'button',
+    loadChildren: () =>
+      import('./button/button.module').then((m) => m.ButtonModule),
+  },
+  {
+    path: 'checkbox',
+    loadChildren: () =>
+      import('./checkbox/checkbox.module').then((m) => m.CheckboxModule),
+  },
+  {
+    path: 'datetime',
+    loadChildren: () =>
+      import('./datetime/datetime.module').then((m) => m.DatetimeModule),
+  },
+  {
+    path: 'list',
+    loadChildren: () => import('./list/list.module').then((m) => m.ListModule),
+  },
+  {
     path: '**',
     redirectTo: '/input',
-   
   },
 ];
 
---
+++ ch.jnetwork.jng-controls/projects/demo-bootstrap4/src/app/button/button.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  JNetworkBootstrap4FormModule,
+  JNetworkBootstrap4ButtonModule,
+  JNetworkBootstrap4ValidationSummaryModule,
+} from '@jnetwork/jngcontrols-bootstrap4';
+import { ButtonRoutingModule } from './button-routing.module';
+import { DemoButtonComponent } from './button.component';
+@NgModule({
+  declarations: [DemoButtonComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonRoutingModule,
+    JNetworkBootstrap4FormModule,
+    JNetworkBootstrap4ButtonModule,
+    JNetworkBootstrap4ValidationSummaryModule,
+    JNetworkBootstrap4ButtonModule,
+  ],
+})
+export class ButtonModule {}

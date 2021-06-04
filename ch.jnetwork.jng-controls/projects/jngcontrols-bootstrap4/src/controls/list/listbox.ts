88c5bceffc4c6b69d52509946d873419fb51d506
philipp.jenni <philipp.jenni@jnetwork.ch>
2021-06-04 11:58:04 +0200

    style: Format Document

--- ch.jnetwork.jng-controls/projects/jngcontrols-bootstrap4/src/controls/list/listbox.ts
+++ ch.jnetwork.jng-controls/projects/jngcontrols-bootstrap4/src/controls/list/listbox.ts
@@ -1,35 +1,58 @@
-import { Component, Host, forwardRef, Injector, Directive, Renderer2, ElementRef, Optional } from '@angular/core';
-import { NG_VALUE_ACCESSOR, ControlContainer, NG_VALIDATORS } from '@angular/forms';
+import {
+  Component,
+  Host,
+  forwardRef,
+  Injector,
+  Directive,
+  Renderer2,
+  ElementRef,
+  Optional,
+} from '@angular/core';
+import {
+  NG_VALUE_ACCESSOR,
+  ControlContainer,
+  NG_VALIDATORS,
+} from '@angular/forms';
 import { NgFormularDirective } from '../form/form';
-import { NgListboxCommon, NgListboxOptionCommon } from '@jnetwork/jngcontrols-common';
-
+import {
+  NgListboxCommon,
+  NgListboxOptionCommon,
+} from '@jnetwork/jngcontrols-common';
 
 @Component({
   selector: 'ng-listbox,ngListbox',
   templateUrl: './listbox.html',
   // Value Access Provider registrieren, damit Wert via Model geschrieben und gelesen werden kann
   providers: [
-    { provide: NG_VALUE_ACCESSOR, multi: true, useExisting: NgListboxComponent },
-    { provide: NG_VALIDATORS, multi: true, useExisting: forwardRef(() => NgListboxComponent) }
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: NgListboxComponent,
+    },
+    {
+      provide: NG_VALIDATORS,
+      multi: true,
+      useExisting: forwardRef(() => NgListboxComponent),
+    },
   ],
   // View Provider, damit das Formular an das Control gebunden werden kann
-  viewProviders: [{ provide: ControlContainer, useExisting: NgFormularDirective }]
+  viewProviders: [
+    { provide: ControlContainer, useExisting: NgFormularDirective },
+  ],
 })
 export class NgListboxComponent extends NgListboxCommon {
-
   constructor(@Host() parent: NgFormularDirective, injector: Injector) {
     super(parent, injector);
   }
-
 }
-
 
 @Directive({ selector: '[ngOption],option' })
 export class NgListboxOptionDirective extends NgListboxOptionCommon {
-
-  constructor(_elementRef: ElementRef, _renderer: Renderer2, @Optional() @Host() listbox: NgListboxComponent) {
+  constructor(
+    _elementRef: ElementRef,
+    _renderer: Renderer2,
+    @Optional() @Host() listbox: NgListboxComponent
+  ) {
     super(_elementRef, _renderer, listbox);
   }
-
 }
-

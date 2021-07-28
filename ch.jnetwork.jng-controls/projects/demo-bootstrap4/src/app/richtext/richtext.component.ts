7523d48b424b0696f7411db744c28779cf6c9faa
philipp.jenni <philipp.jenni@jnetwork.ch>
2021-07-28 14:36:38 +0200

    feat: Zusätzliche Properties in TinyMCE integriert

--- ch.jnetwork.jng-controls/projects/demo-bootstrap4/src/app/richtext/richtext.component.ts
+++ ch.jnetwork.jng-controls/projects/demo-bootstrap4/src/app/richtext/richtext.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, NgZone, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-richtext',
@@ -6,41 +6,63 @@ import { Component, EventEmitter, NgZone, OnInit } from '@angular/core';
   styleUrls: ['./richtext.component.scss'],
 })
 export class DemoRichtextComponent implements OnInit {
+  private dialogsettings = null;
+
   public showdialog = false;
-  public selectedfile = null;
-
+  public selectedfile: string | null = null;
+  public allowedtypes = '';
   public settings: any;
+  public selectedFile: string = null;
 
-  private imageSetter = null;
-
-  private test: EventEmitter<string> = new EventEmitter<string>();
-
-  constructor(public ngZone: NgZone) {
-    this.test.subscribe((log) => {
-      console.log('Emitted');
-    });
-  }
+  constructor(protected ngZone: NgZone) {}
 
   ngOnInit(): void {}
 
   public config = {
     base_url: '/tinymce',
+    skin: 'bootstrap',
+    icons: 'bootstrap',
+    branding: false,
+    height: '400px',
     suffix: '.min',
-    plugins: 'image',
-    toolbar: 'image',
+    plugins: 'image,code,link,media',
+    toolbar: 'image media link | code',
+    document_base_url: '/upload/browser/',
+    file_picker_types: 'file media image',
     file_picker_callback: this.showImageDialog,
     angular: this,
+
+    image_description: true,
+    image_class_list: [
+      { title: 'None', value: '' },
+      { title: 'No border', value: 'img_no_border' },
+      {
+        title: 'Borders',
+        menu: [
+          { title: 'Green border', value: 'img_green_border' },
+          { title: 'Blue border', value: 'img_blue_border' },
+          { title: 'Red border', value: 'img_red_border' },
+        ],
+      },
+    ],
+    image_list: [
+      { title: 'My image 1', value: 'https://www.example.com/my1.gif' },
+      { title: 'My image 2', value: 'http://www.moxiecode.com/my2.gif' },
+    ],
   };
 
-  public showImageDialog(callback, value: string, meta): void {
+  public showImageDialog(callback, value: string, meta: any): void {
     this.settings.angular.ngZone.runOutsideAngular(() => {
       this.settings.angular.ngZone.run(() => {
         this.settings.angular.showdialog = true;
-        this.settings.angular.selectedfile = value.replace(
-          'upload/browser',
-          ''
-        );
-        this.settings.angular.imageSetter = {
+        this.settings.angular.selectedfile = value;
+        if (meta.filetype === 'image') {
+          this.settings.angular.allowedtypes = '.jpg,.gif,.png';
+        } else {
+          this.settings.angular.allowedtypes = null;
+        }
+
+        this.settings.angular.dialogsettings = {
           callback: callback,
           value: value,
           meta: meta,
@@ -49,11 +71,16 @@ export class DemoRichtextComponent implements OnInit {
     });
   }
 
-  public selectedFile: string = null;
-
-  public setImage(): void {
+  public setResult(): void {
     if (this.selectedFile) {
-      this.imageSetter.callback('upload/browser' + this.selectedFile);
+      if (
+        this.selectedFile.startsWith('/') ||
+        this.selectedFile.startsWith('\\')
+      ) {
+        this.selectedFile = this.selectedFile.substring(1);
+      }
+
+      this.dialogsettings.callback(this.selectedFile);
     }
     this.showdialog = false;
   }

f8bf16fa1ec789661e3216b0a7e8145ff669ce7f
Phliipp Jenni <philipp.jenni@jnetwork.ch>
2022-02-07 19:29:57 +0000

    doc: Update Dokumentation

--- ch.jnetwork.jng-controls/documentation/bootstrap4/js/menu-wc.js
+++ ch.jnetwork.jng-controls/documentation/bootstrap4/js/menu-wc.js
@@ -830,15 +830,6 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                         </div>
                         <ul class="links collapse " ${ isNormalMode ? ' id="interfaces-links"' : 'id="xs-interfaces-links"' }>
                             <li class="link">
-                                <a href="interfaces/Chainable.html" data-type="entity-link" >Chainable</a>
-                            </li>
-                            <li class="link">
-                                <a href="interfaces/Chainable-1.html" data-type="entity-link" >Chainable</a>
-                            </li>
-                            <li class="link">
-                                <a href="interfaces/Chainable-2.html" data-type="entity-link" >Chainable</a>
-                            </li>
-                            <li class="link">
                                 <a href="interfaces/ClientRect.html" data-type="entity-link" >ClientRect</a>
                             </li>
                             <li class="link">

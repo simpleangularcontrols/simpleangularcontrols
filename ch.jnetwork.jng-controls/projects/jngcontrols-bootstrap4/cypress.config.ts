3b01b58840e4a52d4d3260803f11ad93f502d476
Phliipp Jenni <philipp.jenni@jnetwork.ch>
2023-02-25 16:48:36 +0100

    refactor: Migration Angular E2E Tests zu Cypress

--- ch.jnetwork.jng-controls/projects/jngcontrols-bootstrap4/cypress.config.ts
+++ ch.jnetwork.jng-controls/projects/jngcontrols-bootstrap4/cypress.config.ts
@@ -1,14 +1,19 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   viewportWidth: 1024,
   viewportHeight: 768,
 
+  e2e: {
+    baseUrl: 'http://localhost:4200',
+    supportFile: false,
+  },
+
   component: {
     devServer: {
-      framework: "angular",
-      bundler: "webpack",
+      framework: 'angular',
+      bundler: 'webpack',
     },
-    specPattern: "**/*.cy.ts",
+    specPattern: '**/*.cy.ts',
   },
 });

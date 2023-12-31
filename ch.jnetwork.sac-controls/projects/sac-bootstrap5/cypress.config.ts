a465de996a61462e2a19e52940504334be56e596
Phliipp Jenni <philipp.jenni@jnetwork.ch>
2023-12-31 13:38:54 +0100

    fix: remove unused e2e cypress config

--- ch.jnetwork.sac-controls/projects/sac-bootstrap5/cypress.config.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap5/cypress.config.ts
@@ -5,11 +5,6 @@ export default defineConfig({
   viewportWidth: 1024,
   viewportHeight: 768,
 
-  e2e: {
-    baseUrl: 'http://localhost:4200',
-    supportFile: false,
-  },
-
   component: {
     devServer: {
       framework: 'angular',

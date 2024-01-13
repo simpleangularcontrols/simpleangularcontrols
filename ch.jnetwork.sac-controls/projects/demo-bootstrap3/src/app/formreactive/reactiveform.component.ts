f6ce03e75755c4b26bc0e2b0e7242a491742446e
Philipp Jenni <philipp.jenni@gmx.ch>
2024-01-13 19:55:58 +0100

    fix: Click on label does not work if property "name" is not defined bug (#47)
    * fix: initalize name property with unique name
    
    * fix: better check if crypto available
    
    ---------
    
    Co-authored-by: Phliipp Jenni <philipp.jenni@jnetwork.ch>

--- ch.jnetwork.sac-controls/projects/demo-bootstrap3/src/app/formreactive/reactiveform.component.ts
+++ ch.jnetwork.sac-controls/projects/demo-bootstrap3/src/app/formreactive/reactiveform.component.ts
@@ -1,20 +1,30 @@
 import { Component } from '@angular/core';
-import { UntypedFormControl, UntypedFormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  UntypedFormControl,
+  UntypedFormGroup,
+} from '@angular/forms';
+import {
+  SACBootstrap3CheckboxModule,
+  SACBootstrap3DateTimeModule,
+  SACBootstrap3InputModule,
+  SACBootstrap3ValidationSummaryModule,
+} from '@simpleangularcontrols/sac-bootstrap3';
 import { Validation } from '@simpleangularcontrols/sac-common';
-import { SACBootstrap3ValidationSummaryModule, SACBootstrap3InputModule, SACBootstrap3CheckboxModule, SACBootstrap3DateTimeModule } from '@simpleangularcontrols/sac-bootstrap3';
 
 @Component({
-    selector: 'app-reactiveform',
-    templateUrl: './reactiveform.component.html',
-    standalone: true,
-    imports: [
-        FormsModule,
-        ReactiveFormsModule,
-        SACBootstrap3ValidationSummaryModule,
-        SACBootstrap3InputModule,
-        SACBootstrap3CheckboxModule,
-        SACBootstrap3DateTimeModule,
-    ],
+  selector: 'app-reactiveform',
+  templateUrl: './reactiveform.component.html',
+  standalone: true,
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    SACBootstrap3ValidationSummaryModule,
+    SACBootstrap3InputModule,
+    SACBootstrap3CheckboxModule,
+    SACBootstrap3DateTimeModule,
+  ],
 })
 export class DemoRectiveFormComponent {
   // formfield1 = new FormControl('', Validation.required('VALIDATION_ERROR_REQUIRED','VALIDATION_ERROR_SUMMARY_REQUIRED'));
@@ -28,6 +38,13 @@ export class DemoRectiveFormComponent {
     partial: new UntypedFormGroup({
       part1: new UntypedFormGroup({
         inputvalue2: new UntypedFormControl(''),
+        inputvalue3: new UntypedFormControl(
+          '',
+          Validation.required(
+            'Custom Error Message',
+            'Custom Error Message (Summary)'
+          )
+        ),
       }),
     }),
   });

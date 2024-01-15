fa53f84bd1f41405655baed903b0e7e028343157
Phliipp Jenni <philipp.jenni@jnetwork.ch>
2024-01-15 19:54:48 +0100

    feat: implement service in demo projects

--- ch.jnetwork.sac-controls/projects/demo-bootstrap5/src/app/app.module.ts
+++ ch.jnetwork.sac-controls/projects/demo-bootstrap5/src/app/app.module.ts
@@ -1,17 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import {
+  SACCONFIGURATION_SERVICE,
+  SACLANGUAGE_SERVICE,
+} from '@simpleangularcontrols/sac-common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppNavigationModule } from './nav/nav.module';
-import { SACLANGUAGE_SERVICE } from '@simpleangularcontrols/sac-common';
 import { ControlsLanguageService } from './services/controlslanguage.service';
+import { CustomConfigurationService } from './services/customconfiguration.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, AppNavigationModule],
   providers: [
     { provide: SACLANGUAGE_SERVICE, useClass: ControlsLanguageService },
+    { provide: SACCONFIGURATION_SERVICE, useClass: CustomConfigurationService },
   ],
   bootstrap: [AppComponent],
 })

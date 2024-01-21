a83ed61aaf588d044d94ef0c4f0fb0147a1ef8fa
Phliipp Jenni <philipp.jenni@jnetwork.ch>
2024-01-21 13:17:35 +0100

    refactor: refactoring of grid controller

--- ch.jnetwork.sac-backend/Api/Services/GridController.cs
+++ ch.jnetwork.sac-backend/Api/Services/GridController.cs
@@ -3,67 +3,88 @@ using SimpleAngularControls.Api.Model;
 
 namespace SimpleAngularControls.Api.Services
 {
+    /// <summary>
+    /// example controlle to demonstrate grid component
+    /// </summary>
     [ApiController]
     [Route("api/grid")]
     public class GridController : ControllerBase
     {
-        public static List<GridItemDto> Items { get; set; } = null;
+        /// <summary>
+        /// static list of grid items
+        /// </summary>
+        public static List<GridItemDto>? Items { get; set; } = null;
 
+        /// <summary>
+        /// get methode for demonstrating grid component
+        /// </summary>
+        /// <param name="request">data with information for sorting and paging</param>
+        /// <returns>return response to display data in grid</returns>
         [Route("items")]
         [HttpPost()]
         public GridResultDto<GridItemDto> GetItems([FromBody] GridRequest request)
         {
-            List<GridItemDto> items;
-
+            // create items if not exists
             if (Items == null)
             {
                 Random random = new Random();
-                items = new List<GridItemDto>();
+                Items = new List<GridItemDto>();
                 for (int i = 1; i < 200; i++)
                 {
-                    items.Add(new GridItemDto()
+                    Items.Add(new GridItemDto()
                     {
                         Id = i,
                         Bezeichnung = $"Element {i}",
                         Image = $"Bild {random.Next(1, 2000)}"
                     });
                 }
-
-                Items = items;
-            }
-            else
-            {
-                items = Items;
             }
 
             int startingPoint = request.NewPageIndex * request.PageSize;
 
-            if (startingPoint > items.Count)
-                startingPoint = items.Count - (items.Count % request.PageSize);
+            if (startingPoint > Items.Count)
+                startingPoint = Items.Count - (Items.Count % request.PageSize);
 
+            // disable sorting if sort key is null
+            if (request.SortKey == null)
+                return new GridResultDto<GridItemDto>()
+                {
+                    TotalRowCount = Items.Count,
+                    Data = Items.Skip(startingPoint).Take(request.PageSize).ToList()
+                };
+
+            // handle sort
             switch (request.SortDirection)
             {
                 case SortOrder.None:
                     return new GridResultDto<GridItemDto>()
                     {
-                        TotalRowCount = items.Count,
-                        Data = items.Skip(startingPoint).Take(request.PageSize).ToList()
+                        TotalRowCount = Items.Count,
+                        Data = Items.Skip(startingPoint).Take(request.PageSize).ToList()
                     };
 
                 case SortOrder.Ascending:
                     var orderItem = typeof(GridItemDto).GetProperty(request.SortKey);
+
+                    if (orderItem == null)
+                        throw new InvalidDataException("SortKey is invalid");
+
                     return new GridResultDto<GridItemDto>()
                     {
-                        TotalRowCount = items.Count,
-                        Data = items.OrderBy(itm => orderItem.GetValue(itm, null)).Skip(startingPoint).Take(request.PageSize).ToList()
+                        TotalRowCount = Items.Count,
+                        Data = Items.OrderBy(itm => orderItem.GetValue(itm, null)).Skip(startingPoint).Take(request.PageSize).ToList()
                     };
 
                 case SortOrder.Descending:
                     var orderItemDescending = typeof(GridItemDto).GetProperty(request.SortKey);
+
+                    if (orderItemDescending == null)
+                        throw new InvalidDataException("SortKey is invalid");
+
                     return new GridResultDto<GridItemDto>()
                     {
-                        TotalRowCount = items.Count,
-                        Data = items.OrderByDescending(itm => orderItemDescending.GetValue(itm, null)).Skip(startingPoint).Take(request.PageSize).ToList()
+                        TotalRowCount = Items.Count,
+                        Data = Items.OrderByDescending(itm => orderItemDescending.GetValue(itm, null)).Skip(startingPoint).Take(request.PageSize).ToList()
                     };
 
                 default:

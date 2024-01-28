5ab753703477b7221ec480d710b1f66134ea28c5
Philipp Jenni <philipp.jenni@jnetwork.ch>
2024-01-28 20:11:06 +0100

    fix: CustomConfigurationService missing @Injectable

--- ch.jnetwork.sac-controls/projects/demo-bootstrap4/src/app/services/customconfiguration.service.ts
+++ ch.jnetwork.sac-controls/projects/demo-bootstrap4/src/app/services/customconfiguration.service.ts
@@ -1,22 +1,35 @@
+import { Injectable } from '@angular/core';
 import { SacAbstractConfigurationService } from '@simpleangularcontrols/sac-common';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class CustomConfigurationService extends SacAbstractConfigurationService {
-  get LabelSizeLg(): number | null {
+  // #region Public Getters And Setters
+
+  public get LabelSizeLg(): number | null {
     return 3;
   }
-  get LabelSizeMd(): number | null {
+
+  public get LabelSizeMd(): number | null {
     return 4;
   }
-  get LabelSizeSm(): number | null {
+
+  public get LabelSizeSm(): number | null {
     return 4;
   }
-  get LabelSizeXl(): number | null {
+
+  public get LabelSizeXl(): number | null {
     return 2;
   }
-  get LabelSizeXs(): number | null {
+
+  public get LabelSizeXs(): number | null {
     return 12;
   }
-  get LabelSizeXxl(): number | null {
+
+  public get LabelSizeXxl(): number | null {
     return 2;
   }
+
+  // #endregion Public Getters And Setters
 }

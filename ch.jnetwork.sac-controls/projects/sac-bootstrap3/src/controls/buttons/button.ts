ea34110899a843e9e991c99864b9f79da9fb5a45
Philipp Jenni <philipp.jenni@exanic.ch>
2024-08-07 17:20:26 +0200

    feat: Add simple spinner implementation in BS3 Button

--- ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/buttons/button.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/buttons/button.ts
@@ -4,7 +4,7 @@ import { SacButtonCommon } from '@simpleangularcontrols/sac-common';
 @Component({
   selector: 'sac-button',
   templateUrl: './button.html',
-  styles: ['sac-button+sac-button {margin-left: 5px}'], // Workaround: Abstand zwischen 2 Buttons setzen, da Boostrap3 nicht mit Host Items umgehen kann
+  styles: ['sac-button+sac-button {margin-left: 5px}', '.spin { animation: spin 1.5s infinite; } @keyframes spin { 0% { transform: rotate(0); } 100% { transform: rotate(360deg); } }'], // Workaround: Abstand zwischen 2 Buttons setzen, da Boostrap3 nicht mit Host Items umgehen kann
   encapsulation: ViewEncapsulation.None, // Workaround: Abstand zwischen 2 Buttons setzen, da Boostrap3 nicht mit Host Items umgehen kann
 })
 export class SacButtonComponent extends SacButtonCommon {}

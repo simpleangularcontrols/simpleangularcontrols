3617ccc21ccb430e564bbd4e6fd389f4842705a0
Philipp Jenni <philipp.jenni@exanic.ch>
2024-08-07 17:03:06 +0200

    fix: Spacing between Buttons in Confirm Dialog cannot be customized

--- ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/buttons/button.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/buttons/button.ts
@@ -1,5 +1,5 @@
 import { CommonModule, NgClass, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { SacButtonCommon } from '@simpleangularcontrols/sac-common';
 
 @Component({
@@ -7,5 +7,7 @@ import { SacButtonCommon } from '@simpleangularcontrols/sac-common';
   templateUrl: './button.html',
   standalone: true,
   imports: [NgIf, NgClass, CommonModule],
+  styles: ['sac-button+sac-button {margin-left: 5px}'], // Workaround: Abstand zwischen 2 Buttons setzen, da Boostrap3 nicht mit Host Items umgehen kann
+  encapsulation: ViewEncapsulation.None, // Workaround: Abstand zwischen 2 Buttons setzen, da Boostrap3 nicht mit Host Items umgehen kann
 })
 export class SacButtonComponent extends SacButtonCommon {}

972db8a8dd9a884e6a6c5e133553185c7533d759
Philipp Jenni <philipp.jenni@exanic.ch>
2024-11-18 09:01:47 +0100

    fix: Date Validation fixed

--- ch.jnetwork.sac-controls/projects/demo-bootstrap3/src/app/formreactive/reactiveform.component.ts
+++ ch.jnetwork.sac-controls/projects/demo-bootstrap3/src/app/formreactive/reactiveform.component.ts
@@ -7,14 +7,16 @@ import { Validation } from '@simpleangularcontrols/sac-common';
   templateUrl: './reactiveform.component.html',
 })
 export class DemoRectiveFormComponent {
+  // #region Properties
+
   // formfield1 = new FormControl('', Validation.required('VALIDATION_ERROR_REQUIRED','VALIDATION_ERROR_SUMMARY_REQUIRED'));
-  baseForm = new FormGroup({
+  public baseForm = new FormGroup({
     inputvalue: new FormControl(''),
     checkboxvalue: new FormControl(true),
     listvalue: new FormControl(''),
     dropdownvalue: new FormControl(''),
     radiobuttonvalue: new FormControl('1'),
-    datetimevalue: new FormControl(''),
+    datetimevalue: new FormControl('2024-11-22T17:10:00+01:00'),
     partial: new FormGroup({
       part1: new FormGroup({
         inputvalue2: new FormControl(''),
@@ -28,4 +30,14 @@ export class DemoRectiveFormComponent {
       }),
     }),
   });
+
+  // #endregion Properties
+
+  // #region Public Methods
+
+  public change(): void {
+    this.baseForm.patchValue({ datetimevalue: '2024-11-23T17:11:00+01:00' });
+  }
+
+  // #endregion Public Methods
 }
--- ch.jnetwork.sac-controls/projects/sac-common/src/validation/isDateValid.ts
+++ ch.jnetwork.sac-controls/projects/sac-common/src/validation/isDateValid.ts
@@ -1,13 +1,25 @@
-import { Moment } from 'moment';
 import * as moment_ from 'moment';
+import { Moment } from 'moment';
+
+// #region Variables
+
 /**
  * Moment
  */
 export const moment = moment_['default'];
 
+// #endregion Variables
+
+// #region Functions
+
 export function isDateValid(value: any, format: string) {
   // NULL ist gültig
   if (value === null || value === undefined || value === '') {
+    return true;
+  }
+
+  // Check is Iso Date (From API Call)
+  if (moment(value, moment_.ISO_8601, true).isValid()) {
     return true;
   }
 
@@ -16,3 +28,6 @@ export function isDateValid(value: any, format: string) {
 
   return date.isValid();
 }
+
+// #endregion Functions
+

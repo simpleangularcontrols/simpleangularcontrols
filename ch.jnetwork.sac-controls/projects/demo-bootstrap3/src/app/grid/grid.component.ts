1918a932550619c2fd9227bb83213afb4ed2c620
Philipp Jenni <philipp.jenni@exanic.ch>
2025-01-07 18:38:57 +0100

    fix (#157): Pager should not be visible if pagerdata is null

--- ch.jnetwork.sac-controls/projects/demo-bootstrap3/src/app/grid/grid.component.ts
+++ ch.jnetwork.sac-controls/projects/demo-bootstrap3/src/app/grid/grid.component.ts
@@ -19,57 +19,85 @@ import { GridService } from './services/GridService';
   imports: [SACBootstrap3GridModule, HttpClientModule],
 })
 export class DemoGridComponent implements OnInit {
-  dataDefault: GridItemDto[] = [];
-  pagerDefault: PagerData = {
+  // #region Properties
+
+  public dataDefault: GridItemDto[] = [];
+  public dataPageSize: GridItemDto[] = [];
+  public dataPageSizeDisabled: GridItemDto[] = [];
+  public dataPagingDisabled: GridItemDto[] = [];
+  public dataSorting: GridItemDto[] = [];
+  public pagerDefault: PagerData = {
     TotalRowCount: 0,
     CurrentPageIndex: 0,
     PageSize: 20,
   };
-
-  dataPageSize: GridItemDto[] = [];
-  pagerPageSize: PagerData = {
+  public pagerPageSize: PagerData = {
     TotalRowCount: 0,
     CurrentPageIndex: 0,
     PageSize: 20,
   };
-
-  dataPageSizeDisabled: GridItemDto[] = [];
-  pagerPageSizeDisabled: PagerData = {
+  public pagerPageSizeDisabled: PagerData = {
     TotalRowCount: 0,
     CurrentPageIndex: 0,
     PageSize: 10,
   };
-
-  dataSorting: GridItemDto[] = [];
-  pagerSorting: PagerData = {
+  public pagerSorting: PagerData = {
     TotalRowCount: 0,
     CurrentPageIndex: 0,
     PageSize: 20,
   };
-  sortSorting: SortDescriptor = {
+  public sortSorting: SortDescriptor = {
     SortColumn: 'Id',
     SortOrder: SortOrder.Ascending,
   };
 
+  // #endregion Properties
+
+  // #region Constructors
+
   constructor(private gridService: GridService) {}
 
-  pagingDefault(pageRequest: PagerRequest) {
+  // #endregion Constructors
+
+  // #region Public Methods
+
+  public action(value: any) {
+    console.log('ExampleGrid: action -> ' + JSON.stringify(value));
+
+    // this.dataDefault.CurrentPageIndex++;
+    // this.dataDefault = this.dataDefault;
+  }
+
+  public ngOnInit(): void {
+    const request: PagerRequest = new PagerRequest(20, 0);
+    this.loadDefault(request);
+    this.loadPageSize(request);
+    this.loadSorting(request, this.sortSorting);
+    this.loadPageSizeDisabled({ ...request, PageSize: 10 });
+    this.loadPagingDisabled(null);
+  }
+
+  public pagingDefault(pageRequest: PagerRequest) {
     this.loadDefault(pageRequest);
   }
 
-  pagingPageSize(pageRequest: PagerRequest) {
+  public pagingDisabled(pageRequest: PagerRequest) {
+    this.loadPagingDisabled(pageRequest);
+  }
+
+  public pagingPageSize(pageRequest: PagerRequest) {
     this.loadPageSize(pageRequest);
   }
 
-  pagingPageSizeDisabled(pageRequest: PagerRequest) {
+  public pagingPageSizeDisabled(pageRequest: PagerRequest) {
     this.loadPageSizeDisabled(pageRequest);
   }
 
-  pagingSorting(pageRequest: PagerRequest) {
+  public pagingSorting(pageRequest: PagerRequest) {
     this.loadSorting(pageRequest, this.sortSorting);
   }
 
-  sortingSort(sortRequest: SortDescriptor) {
+  public sortingSort(sortRequest: SortDescriptor) {
     this.loadSorting(
       new PagerRequest(
         this.pagerSorting.PageSize,
@@ -79,13 +107,9 @@ export class DemoGridComponent implements OnInit {
     );
   }
 
-  ngOnInit(): void {
-    const request: PagerRequest = new PagerRequest(20, 0);
-    this.loadDefault(request);
-    this.loadPageSize(request);
-    this.loadSorting(request, this.sortSorting);
-    this.loadPageSizeDisabled({ ...request, PageSize: 10 });
-  }
+  // #endregion Public Methods
+
+  // #region Private Methods
 
   private loadDefault(pageRequest: PagerRequest) {
     const request: GridRequestDto = new GridRequestDto();
@@ -141,6 +165,18 @@ export class DemoGridComponent implements OnInit {
       });
   }
 
+  private loadPagingDisabled(pageRequest: PagerRequest) {
+    const request: GridRequestDto = new GridRequestDto();
+    request.NewPageIndex = 1;
+    request.PageSize = 999999;
+
+    this.gridService
+      .GetItems(request)
+      .subscribe((result: GridResultDto<GridItemDto>) => {
+        this.dataPagingDisabled = result.Data;
+      });
+  }
+
   private loadSorting(pageRequest: PagerRequest, sortRequest: SortDescriptor) {
     const request: GridRequestDto = new GridRequestDto();
     request.NewPageIndex = pageRequest.NewPageIndex;
@@ -166,10 +202,5 @@ export class DemoGridComponent implements OnInit {
       });
   }
 
-  public action(value: any) {
-    console.log('ExampleGrid: action -> ' + JSON.stringify(value));
-
-    // this.dataDefault.CurrentPageIndex++;
-    // this.dataDefault = this.dataDefault;
-  }
+  // #endregion Private Methods
 }

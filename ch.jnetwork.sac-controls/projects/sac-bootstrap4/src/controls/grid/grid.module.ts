c603692c6b4f2bfc92a609214eb66f6c2e1412ca
Philipp Jenni <philipp.jenni@exanic.ch>
2025-01-13 16:53:01 +0100

    feat (#171): PageSize support for bootstrap3 grid

--- ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/grid/grid.module.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/grid/grid.module.ts
@@ -1,6 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { SacGridComponent } from './grid';
 import { SacGridButtonComponent } from './gridbutton';
 import { SacGridColumnComponent } from './gridcolumn';
@@ -8,13 +8,23 @@ import { SacGridColumnActionComponent } from './gridcolumnaction';
 import { SacGridImageComponent } from './gridimage';
 import { SacPagingComponent } from './paging';
 
-
 @NgModule({
-  declarations: [SacGridComponent, SacGridColumnComponent, SacGridColumnActionComponent, SacPagingComponent, SacGridButtonComponent, SacGridImageComponent],
-  imports: [
-    CommonModule, FormsModule
+  declarations: [
+    SacGridComponent,
+    SacGridColumnComponent,
+    SacGridColumnActionComponent,
+    SacPagingComponent,
+    SacGridButtonComponent,
+    SacGridImageComponent,
   ],
-  exports: [SacGridComponent, SacGridColumnComponent, SacGridColumnActionComponent, SacPagingComponent, SacGridButtonComponent, SacGridImageComponent]
+  imports: [CommonModule, FormsModule],
+  exports: [
+    SacGridComponent,
+    SacGridColumnComponent,
+    SacGridColumnActionComponent,
+    SacPagingComponent,
+    SacGridButtonComponent,
+    SacGridImageComponent,
+  ],
 })
-export class SACBootstrap4GridModule { }
-
+export class SACBootstrap4GridModule {}

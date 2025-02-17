54825e707510ee7826b1b60d4c54ac66b355afe1
Philipp Jenni <philipp.jenni@exanic.ch>
2025-02-17 13:05:12 +0100

    fix (#199): Dropdown Emtpy cannot be numeric

---
+++ ch.jnetwork.sac-controls/projects/sac-common/src/controls/list/buildvaluestring.ts
@@ -0,0 +1,19 @@
+/**
+ * Function um ein Key Value Pair für das Dropdown zu erzeugen
+ * @param id ID
+ * @param value Wert der an das Element gebunden werden soll
+ */
+export function _buildValueString(id: string | null, value: any): string {
+  // Wenn ID null ist Object zurückgeben
+  if (id == null) {
+    return `${value}`;
+  }
+
+  // Mapping Objekt zu String
+  if (value && typeof value === 'object') {
+    value = 'Object';
+  }
+
+  // String als ID
+  return `${id}: ${value}`.slice(0, 50);
+}

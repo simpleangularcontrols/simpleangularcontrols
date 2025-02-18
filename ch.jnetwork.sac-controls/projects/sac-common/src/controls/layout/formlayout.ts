db07fb970e4a5fc6ec947990e0d19ed2d4704b0c
Philipp Jenni <philipp.jenni@gmx.ch>
2025-02-18 10:57:40 +0100

    feat: Different alignment of label and help text tooltip enhancement New feature or request (#204)
    * feat (#201): Allow split label and tooltip helptext
    
    * doc: Extend CompoDoc with advanced documentation
    
    * fix: Build Issue after Merge from Angular 10 Release
    
    * refactor: Configure Document Formatting
    
    * fix: Style Issue solved

--- ch.jnetwork.sac-controls/projects/sac-common/src/controls/layout/formlayout.ts
+++ ch.jnetwork.sac-controls/projects/sac-common/src/controls/layout/formlayout.ts
@@ -6,69 +6,80 @@ import { ControlHeight } from '../../enums/ControlHeight';
  */
 @Directive()
 export class SacFormLayoutCommon {
-  // #region Properties
-
   /**
    * Defines the default display for a checkbox. You can choose between a checkbox and a switch. If no style is defined, the global style or the style on the control is used.
    */
   @Input()
   public checkboxstyle: 'checkbox' | 'switch' | null = null;
+
   /**
    * Defines the standard height of the components
    */
   @Input() public componentHeight: ControlHeight | null = null;
+
   /**
    * Mode for display helptext
    */
   @Input()
   public helptextmode: 'tooltip' | 'text' | null;
+
   /**
    * defines that error messages are displayed under the controls
    */
   @Input() public inlineError: boolean | null = null;
+
   /**
    * Defines if InputSearch uses an icon at the button or the text
    */
   @Input()
   public inputsearchiconmode: 'text' | 'icon' | 'mixed' | null = null;
+
   /**
    * defines that the labels are displayed as adaptive labels
    */
   @Input() public isAdaptiveLabel: boolean = false;
+
   /**
    * default labe size for large devices
    */
   @Input()
   public labelSizeLg: number | null = null;
+
   /**
    * default label size for medium devices
    */
   @Input()
   public labelSizeMd: number | null = null;
+
   /**
    * default label size for small devices
    */
   @Input()
   public labelSizeSm: number | null = null;
+
   /**
    * default label size for extra large devices
    */
   @Input()
   public labelSizeXl: number | null = null;
+
   /**
    * default label column size
    */
   @Input()
   public labelSizeXs: number | null = null;
+
   /**
    * default label size for extra extra large devices
    */
   @Input()
   public labelSizeXxl: number | null = null;
 
-  // #endregion Properties
-
-  // #region Public Getters And Setters
+  /**
+   * Detach label text and tooltip from each other in Label so that label and tooltip can be aligned differently. This is in Bootstrap 3 not supported!
+   */
+  @Input()
+  public splitlabelandhelptext: boolean | null = null;
 
   /**
    * Returns whether the inline error messages for the form are active.
@@ -76,6 +87,4 @@ export class SacFormLayoutCommon {
   public get IsInlineErrorEnabled(): boolean {
     return this.inlineError;
   }
-
-  // #endregion Public Getters And Setters
 }
--- ch.jnetwork.sac-controls/projects/sac-common/src/interfaces/ISacConfigurationService.ts
+++ ch.jnetwork.sac-controls/projects/sac-common/src/interfaces/ISacConfigurationService.ts
@@ -4,56 +4,68 @@ import { ControlHeight } from '../enums/ControlHeight';
  * interface for service which provides global configuration settings to components
  */
 export interface ISacConfigurationService {
-  // #region Public Getters And Setters
-
   /**
    * Defines the default display for a checkbox. You can choose between a checkbox and a switch.
    */
   CheckboxStyle: 'checkbox' | 'switch';
+
   /**
    * Defines the standard height of the components
    */
-  get ComponentHeight(): ControlHeight | null;
+  ComponentHeight: ControlHeight | null;
+
   /**
    * Defines the standard text for currencies. e.g. $, €, CHF
    */
-  get CurrencyText(): string;
+  CurrencyText: string;
+
   /**
    * Mode for display helptext
    */
   HelptextMode: 'tooltip' | 'text';
+
   /**
    * Activates the error messages below the controls
    */
   InlineErrorEnabled: boolean;
+
   /**
    * Defines if InputSearch uses an icon at the button or the text
    */
   InputSearchIconMode: 'text' | 'icon' | 'mixed';
+
   /**
    * default labe size for large devices
    */
-  get LabelSizeLg(): number | null;
+  LabelSizeLg: number | null;
+
   /**
    * default label size for medium devices
    */
-  get LabelSizeMd(): number | null;
+  LabelSizeMd: number | null;
+
   /**
    * default label size for small devices
    */
-  get LabelSizeSm(): number | null;
+  LabelSizeSm: number | null;
+
   /**
    * default label size for extra large devices
    */
-  get LabelSizeXl(): number | null;
+  LabelSizeXl: number | null;
+
   /**
    * default label column size
    */
-  get LabelSizeXs(): number | null;
+  LabelSizeXs: number | null;
+
   /**
    * default label size for extra extra large devices
    */
   get LabelSizeXxl(): number | null;
 
-  // #endregion Public Getters And Setters
+  /**
+   * Detach label text and tooltip from each other in Label so that label and tooltip can be aligned differently. This is in Bootstrap 3 not supported!
+   */
+  SplitLabelAndHelptext: boolean;
 }

74d0c6b959e8ae0b20744f0ec6680328e25cd258
Philipp Jenni <philipp.jenni@exanic.ch>
2025-02-19 10:17:31 +0100

    feat: Tooltip support with disablelabel in BS4

--- ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/static/formcontainer.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/static/formcontainer.ts
@@ -1,7 +1,7 @@
+import { SacFormLayoutDirective } from '../layout/formlayout.directive';
 import { Component, Host, Injector, Optional, forwardRef } from '@angular/core';
 import { NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { SacStaticFormContainerCommon } from '@simpleangularcontrols/sac-common';
-import { SacFormLayoutDirective } from '../layout/formlayout.directive';
 
 /**
  * Component für einbindung eines beliebigen Controls in die Form Struktur
@@ -18,36 +18,33 @@ import { SacFormLayoutDirective } from '../layout/formlayout.directive';
  *
  **/
 @Component({
-  selector: 'sac-staticformcontainer',
-  templateUrl: './formcontainer.html',
-  // Value Access Provider registrieren, damit Wert via Model geschrieben und gelesen werden kann
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      multi: true,
-      useExisting: SacStaticFormContainerComponent,
-    },
-    {
-      provide: NG_VALIDATORS,
-      multi: true,
-      useExisting: forwardRef(() => SacStaticFormContainerComponent),
-    },
-  ],
+    selector: 'sac-staticformcontainer',
+    templateUrl: './formcontainer.html',
+    // Value Access Provider registrieren, damit Wert via Model geschrieben und gelesen werden kann
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: SacStaticFormContainerComponent,
+        },
+        {
+            provide: NG_VALIDATORS,
+            multi: true,
+            useExisting: forwardRef(() => SacStaticFormContainerComponent),
+        },
+    ],
 })
 export class SacStaticFormContainerComponent extends SacStaticFormContainerCommon {
-  // #region Constructors
+    // #region Constructors
 
-  /**
-   *
-   * @param formLayout SacFormLayout to define scoped layout settings
-   * @param injector Injector for injecting services
-   */
-  constructor(
-    @Host() @Optional() formLayout: SacFormLayoutDirective,
-    injector: Injector
-  ) {
-    super(formLayout, injector);
-  }
+    /**
+     *
+     * @param formLayout SacFormLayout to define scoped layout settings
+     * @param injector Injector for injecting services
+     */
+    constructor(@Host() @Optional() formLayout: SacFormLayoutDirective, injector: Injector) {
+        super(formLayout, injector);
+    }
 
-  // #endregion Constructors
+    // #endregion Constructors
 }

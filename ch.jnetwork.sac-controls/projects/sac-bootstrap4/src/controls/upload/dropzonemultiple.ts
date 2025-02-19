c772122b4791e4c090ce7c0d45aa972ef34c4a6e
Philipp Jenni <philipp.jenni@exanic.ch>
2025-02-19 11:50:45 +0100

    feat: Tooltip Helptext with disablelabel in BS4 Dropzone

--- ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/upload/dropzonemultiple.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/upload/dropzonemultiple.ts
@@ -1,70 +1,58 @@
-import {
-  Component,
-  forwardRef,
-  Host,
-  Injector,
-  NgZone,
-  OnInit,
-  Optional,
-  Renderer2,
-} from '@angular/core';
+import { SacFormLayoutDirective } from '../layout/formlayout.directive';
+import { Component, Host, Injector, NgZone, OnInit, Optional, Renderer2, forwardRef } from '@angular/core';
 import { NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { SacDropzoneMultipleCommon } from '@simpleangularcontrols/sac-common';
-import { SacFormLayoutDirective } from '../layout/formlayout.directive';
 
 /**
  * Dropzone Komponente für mehrere Uploads
  * @see https://github.com/kukhariev/ngx-uploadx/
  */
 @Component({
-  selector: 'sac-dropzonemultiple',
-  templateUrl: './dropzonemultiple.html',
-  styleUrls: ['./dropzone.scss'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      multi: true,
-      useExisting: SacDropzoneMultipleComponent,
-    },
-    {
-      provide: NG_VALIDATORS,
-      multi: true,
-      useExisting: forwardRef(() => SacDropzoneMultipleComponent),
-    },
-  ],
+    selector: 'sac-dropzonemultiple',
+    templateUrl: './dropzonemultiple.html',
+    styleUrls: ['./dropzone.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: SacDropzoneMultipleComponent,
+        },
+        {
+            provide: NG_VALIDATORS,
+            multi: true,
+            useExisting: forwardRef(() => SacDropzoneMultipleComponent),
+        },
+    ],
 })
-export class SacDropzoneMultipleComponent
-  extends SacDropzoneMultipleCommon
-  implements OnInit
-{
-  // #region Constructors
+export class SacDropzoneMultipleComponent extends SacDropzoneMultipleCommon implements OnInit {
+    // #region Constructors
 
-  /**
-   * Constructor
-   * @param formLayout SacFormLayout to define scoped layout settings
-   * @param injector Injector for injecting services
-   * @param renderer Angular rendering engine
-   * @param ngZone ngZone to manage external javascripts
-   */
-  constructor(
-    @Host() @Optional() formLayout: SacFormLayoutDirective,
-    injector: Injector,
-    renderer: Renderer2,
-    ngZone: NgZone
-  ) {
-    super(formLayout, injector, renderer, ngZone);
-  }
+    /**
+     * Constructor
+     * @param formLayout SacFormLayout to define scoped layout settings
+     * @param injector Injector for injecting services
+     * @param renderer Angular rendering engine
+     * @param ngZone ngZone to manage external javascripts
+     */
+    constructor(
+        @Host() @Optional() formLayout: SacFormLayoutDirective,
+        injector: Injector,
+        renderer: Renderer2,
+        ngZone: NgZone
+    ) {
+        super(formLayout, injector, renderer, ngZone);
+    }
 
-  // #endregion Constructors
+    // #endregion Constructors
 
-  // #region Public Methods
+    // #region Public Methods
 
-  /**
-   * Initialisiert das Control
-   */
-  public ngOnInit() {
-    super.ngOnInit();
-  }
+    /**
+     * Initialisiert das Control
+     */
+    public ngOnInit() {
+        super.ngOnInit();
+    }
 
-  // #endregion Public Methods
+    // #endregion Public Methods
 }

6fe82481cd8118a5cfa28b09ec1d9695450f4f30
Philipp Jenni <philipp.jenni@gmx.ch>
2025-02-20 14:53:10 +0100

    fix: Help text of type Tooltip cannot be used with DisableLabel (#215)
    * feat: Tooltip Helptext with disabled label (BS3)
    
    * feat: Extend Demo Pages for Helptext Tooltip without Label (BS4 and BS5)
    
    * feat: Add Tooltip with Required Property
    
    * fix: style issue with validator message solved
    
    * fix: Import in BS4 and BS5 Demo changed
    
    * refactor: Format Demo ConfigurationServices
    
    * feat: Tooltip support with disablelabel in BS4
    
    * feat: Tooltip Helptext with disablelabel in BS4 Dropzone
    
    * fix: Range Class in Demo BS5 solved
    
    * feat: Tooltip Helptext in BS5 components implemented
    
    * fix: Label Style in Radiobutton solved
    
    * fix: Remove formGroupName in Input Demo
    
    * fix: Label Issue in BS5 Checkbox and Radiobutton

--- ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/checkbox/radiobutton.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/checkbox/radiobutton.ts
@@ -1,21 +1,32 @@
-import { Component, Host } from '@angular/core';
-import { SacRadiobuttonCommon } from '@simpleangularcontrols/sac-common';
+import { SacFormLayoutDirective } from '../layout/formlayout.directive';
 import { SacRadiobuttonsComponent } from './radiobuttons';
+import { Component, Host, Injector, Optional } from '@angular/core';
+import { SacRadiobuttonCommon } from '@simpleangularcontrols/sac-common';
 
 /**
  * Radiobutton Komponente
  */
 @Component({
-  selector: 'sac-radiobutton',
-  templateUrl: './radiobutton.html',
-  styleUrls: ['./radiobutton.scss'],
+    selector: 'sac-radiobutton',
+    templateUrl: './radiobutton.html',
+    styleUrls: ['./radiobutton.scss'],
 })
 export class SacRadiobuttonComponent extends SacRadiobuttonCommon {
-  /**
-   * Konstruktor
-   * @param SacRadioButtons Radio Buttons Group Komponente
-   */
-  constructor(@Host() sacRadioButtons: SacRadiobuttonsComponent) {
-    super(sacRadioButtons);
-  }
+    // #region Constructors
+
+    /**
+     * Konstruktor
+     * @param SacRadioButtons Radio Buttons Group Komponente
+     * @param formlayout SacFormLayoutCommon to define scoped layout settings
+     * @param injector Injector for injecting services
+     */
+    constructor(
+        @Host() sacRadioButtons: SacRadiobuttonsComponent,
+        @Host() @Optional() formLayout: SacFormLayoutDirective,
+        injector: Injector
+    ) {
+        super(sacRadioButtons, formLayout, injector);
+    }
+
+    // #endregion Constructors
 }
--- ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/upload/uploadmultiple.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/upload/uploadmultiple.ts
@@ -1,55 +1,47 @@
-import {
-  Component,
-  forwardRef,
-  Host,
-  Injector,
-  NgZone,
-  Optional,
-  Renderer2,
-} from '@angular/core';
+import { SacFormLayoutDirective } from '../layout/formlayout.directive';
+import { Component, Host, Injector, NgZone, Optional, Renderer2, forwardRef } from '@angular/core';
 import { NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { SacUploadMultipleCommon } from '@simpleangularcontrols/sac-common';
-import { SacFormLayoutDirective } from '../layout/formlayout.directive';
 
 /**
  * Upload Komponente für den Upload mehrer Files
  * @see https://github.com/kukhariev/ngx-uploadx/
  */
 @Component({
-  selector: 'sac-uploadmultiple',
-  templateUrl: './uploadmultiple.html',
-  styleUrls: ['./upload.scss'],
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      multi: true,
-      useExisting: SacUploadMultipleComponent,
-    },
-    {
-      provide: NG_VALIDATORS,
-      multi: true,
-      useExisting: forwardRef(() => SacUploadMultipleComponent),
-    },
-  ],
+    selector: 'sac-uploadmultiple',
+    templateUrl: './uploadmultiple.html',
+    styleUrls: ['./upload.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: SacUploadMultipleComponent,
+        },
+        {
+            provide: NG_VALIDATORS,
+            multi: true,
+            useExisting: forwardRef(() => SacUploadMultipleComponent),
+        },
+    ],
 })
 export class SacUploadMultipleComponent extends SacUploadMultipleCommon {
-  // #region Constructors
+    // #region Constructors
 
-  /**
-   * Constructor
-   * @param formLayout SacFormLayout to define scoped layout settings
-   * @param injector Injector for injecting services
-   * @param renderer Angular rendering engine
-   * @param ngZone ngZone to manage external javascripts
-   */
-  constructor(
-    @Host() @Optional() formLayout: SacFormLayoutDirective,
-    injector: Injector,
-    renderer: Renderer2,
-    ngZone: NgZone
-  ) {
-    super(formLayout, injector, renderer, ngZone);
-  }
+    /**
+     * Constructor
+     * @param formLayout SacFormLayout to define scoped layout settings
+     * @param injector Injector for injecting services
+     * @param renderer Angular rendering engine
+     * @param ngZone ngZone to manage external javascripts
+     */
+    constructor(
+        @Host() @Optional() formLayout: SacFormLayoutDirective,
+        injector: Injector,
+        renderer: Renderer2,
+        ngZone: NgZone
+    ) {
+        super(formLayout, injector, renderer, ngZone);
+    }
 
-  // #endregion Constructors
+    // #endregion Constructors
 }

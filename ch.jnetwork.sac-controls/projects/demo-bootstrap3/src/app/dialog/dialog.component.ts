36ee8c1f81dd0b8b6f247bbdc3e4d8937d51e036
Philipp Jenni <philipp.jenni@exanic.ch>
2025-02-21 09:29:46 +0100

    fix: Add Example Code to Test the Bug

--- ch.jnetwork.sac-controls/projects/demo-bootstrap3/src/app/dialog/dialog.component.ts
+++ ch.jnetwork.sac-controls/projects/demo-bootstrap3/src/app/dialog/dialog.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
-import { SACBootstrap3FormModule, SACBootstrap3ButtonModule, SACBootstrap3DialogModule } from '@simpleangularcontrols/sac-bootstrap3';
 import { FormsModule } from '@angular/forms';
+import {
+    SACBootstrap3ButtonModule,
+    SACBootstrap3DateTimeModule,
+    SACBootstrap3DialogModule,
+    SACBootstrap3FormModule,
+} from '@simpleangularcontrols/sac-bootstrap3';
 
 @Component({
     selector: 'app-dialog',
@@ -11,26 +16,35 @@ import { FormsModule } from '@angular/forms';
         SACBootstrap3FormModule,
         SACBootstrap3ButtonModule,
         SACBootstrap3DialogModule,
+        SACBootstrap3DateTimeModule,
     ],
 })
 export class DemoDialogComponent {
-  public isvisible1 = false;
+    // #region Properties
 
-  public showVisibile1(): void {
-    this.isvisible1 = true;
-  }
+    public datevalue = new Date().getDate();
+    public isvisible1 = false;
+    public isvisible2 = false;
 
-  public hideVisibile1() {
-    this.isvisible1 = false;
-  }
+    // #endregion Properties
 
-  public isvisible2 = false;
+    // #region Public Methods
 
-  public showVisibile2(): void {
-    this.isvisible2 = true;
-  }
+    public hideVisibile1() {
+        this.isvisible1 = false;
+    }
 
-  public hideVisibile2() {
-    this.isvisible2 = false;
-  }
+    public hideVisibile2() {
+        this.isvisible2 = false;
+    }
+
+    public showVisibile1(): void {
+        this.isvisible1 = true;
+    }
+
+    public showVisibile2(): void {
+        this.isvisible2 = true;
+    }
+
+    // #endregion Public Methods
 }

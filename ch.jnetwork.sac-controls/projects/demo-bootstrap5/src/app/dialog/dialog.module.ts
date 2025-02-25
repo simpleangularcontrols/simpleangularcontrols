09a93f721cd2e4ee8753500b876562100642a7f2
Philipp Jenni <philipp.jenni@gmx.ch>
2025-02-25 09:59:02 +0100

    feat: DateTime Controls refactored (#223)
    * fix: Add Example Code to Test the Bug
    
    * refactor: Refactoring DateTime base classes
    
    * refactor: Popup logic moved from tooltip to external class
    
    * feat: Add TopEnd and BottomEnd Positon to PopupHelper
    
    * feat: Change Popup Position in DateTime Controls
    
    should close (#217) and (#168)
    
    * fix: Calc of position is wrong when position fixed

--- ch.jnetwork.sac-controls/projects/demo-bootstrap5/src/app/dialog/dialog.module.ts
+++ ch.jnetwork.sac-controls/projects/demo-bootstrap5/src/app/dialog/dialog.module.ts
@@ -1,25 +1,28 @@
+import { DialogRoutingModule } from './dialog-routing.module';
+import { DemoDialogComponent } from './dialog.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
-  SACBootstrap5ButtonModule,
-  SACBootstrap5DialogModule,
-  SACBootstrap5FormModule,
-  SACBootstrap5ValidationSummaryModule,
-} from '@simpleangularcontrols/sac-bootstrap5';
-import { DialogRoutingModule } from './dialog-routing.module';
-import { DemoDialogComponent } from './dialog.component';
-@NgModule({
-  declarations: [DemoDialogComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    DialogRoutingModule,
-    SACBootstrap5FormModule,
-    SACBootstrap5DialogModule,
-    SACBootstrap5ValidationSummaryModule,
     SACBootstrap5ButtonModule,
+    SACBootstrap5DateTimeModule,
     SACBootstrap5DialogModule,
-  ],
+    SACBootstrap5FormModule,
+    SACBootstrap5ValidationSummaryModule,
+} from '@simpleangularcontrols/sac-bootstrap5';
+
+@NgModule({
+    declarations: [DemoDialogComponent],
+    imports: [
+        CommonModule,
+        FormsModule,
+        DialogRoutingModule,
+        SACBootstrap5FormModule,
+        SACBootstrap5DialogModule,
+        SACBootstrap5DateTimeModule,
+        SACBootstrap5ValidationSummaryModule,
+        SACBootstrap5ButtonModule,
+        SACBootstrap5DialogModule,
+    ],
 })
 export class DialogModule {}
--- ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/datetime/datetime.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/datetime/datetime.ts
@@ -1,48 +1,43 @@
-import {
-  Component,
-  ElementRef,
-  Host,
-  Injector,
-  Optional,
-  forwardRef,
-} from '@angular/core';
+import { SacFormLayoutDirective } from '../layout/formlayout.directive';
+import { ChangeDetectorRef, Component, ElementRef, Host, Injector, Optional, forwardRef } from '@angular/core';
 import { NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { SacDateTimeCommon } from '@simpleangularcontrols/sac-common';
-import { SacFormLayoutDirective } from '../layout/formlayout.directive';
 
 @Component({
-  selector: 'sac-datetime',
-  templateUrl: './datetime.html',
-  // Value Access Provider registrieren, damit Wert via Model geschrieben und gelesen werden kann
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      multi: true,
-      useExisting: forwardRef(() => SacDateTimeComponent),
-    },
-    {
-      provide: NG_VALIDATORS,
-      multi: true,
-      useExisting: forwardRef(() => SacDateTimeComponent),
-    },
-  ],
+    selector: 'sac-datetime',
+    templateUrl: './datetime.html',
+    // Value Access Provider registrieren, damit Wert via Model geschrieben und gelesen werden kann
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: forwardRef(() => SacDateTimeComponent),
+        },
+        {
+            provide: NG_VALIDATORS,
+            multi: true,
+            useExisting: forwardRef(() => SacDateTimeComponent),
+        },
+    ],
 })
 export class SacDateTimeComponent extends SacDateTimeCommon {
-  // #region Constructors
+    // #region Constructors
 
-  /**
-   * Constructor
-   * @param formLayout SacFormLayout to define scoped layout settings
-   * @param injector Injector for injecting services
-   * @param elementRef Reference to html dom element
-   */
-  constructor(
-    @Host() @Optional() formLayout: SacFormLayoutDirective,
-    injector: Injector,
-    elementRef: ElementRef
-  ) {
-    super(formLayout, injector, elementRef);
-  }
+    /**
+     * Constructor
+     * @param formLayout SacFormLayout to define scoped layout settings
+     * @param injector Injector for injecting services
+     * @param elementRef Reference to html dom element
+     * @param cdRef  Change Dectection Servie
+     */
+    constructor(
+        @Host() @Optional() formLayout: SacFormLayoutDirective,
+        injector: Injector,
+        elementRef: ElementRef,
+        cdRef: ChangeDetectorRef
+    ) {
+        super(formLayout, injector, elementRef, cdRef);
+    }
 
-  // #endregion Constructors
+    // #endregion Constructors
 }

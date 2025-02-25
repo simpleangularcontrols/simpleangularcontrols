867608e1fbc17e81f5fd2cafc0181a0cd59639ab
Philipp Jenni <philipp.jenni@exanic.ch>
2025-02-25 16:20:40 +0100

    feat: Add Dropzone for Boostrap3
    fix (#106)

--- ch.jnetwork.sac-controls/projects/demo-bootstrap3/src/app/uploader/uploader.component.ts
+++ ch.jnetwork.sac-controls/projects/demo-bootstrap3/src/app/uploader/uploader.component.ts
@@ -1,76 +1,107 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
-  SACBootstrap3ButtonModule,
-  SACBootstrap3FormModule,
-  SACBootstrap3UploadModule,
-  SACBootstrap3ValidationSummaryModule,
-  SacFormDirective,
+    SACBootstrap3ButtonModule,
+    SACBootstrap3FormModule,
+    SACBootstrap3UploadModule,
+    SACBootstrap3ValidationSummaryModule,
+    SacFormDirective,
 } from '@simpleangularcontrols/sac-bootstrap3';
 
-interface KeyValue {
-  label: string;
-  value: string;
-  text: string;
-}
+// #region Interfaces
 
-interface KeyValueNumeric {
-  label: string;
-  value: number;
-  text: string;
+interface KeyValue {
+    // #region Properties
+
+    label: string;
+    text: string;
+    value: string;
+
+    // #endregion Properties
 }
 
 interface KeyValue2 extends KeyValue {
-  enabled: boolean;
+    // #region Properties
+
+    enabled: boolean;
+
+    // #endregion Properties
 }
 
 interface KeyValue3 extends KeyValue2 {
-  checked: boolean;
+    // #region Properties
+
+    checked: boolean;
+
+    // #endregion Properties
 }
+
+interface KeyValueNumeric {
+    // #region Properties
+
+    label: string;
+    text: string;
+    value: number;
+
+    // #endregion Properties
+}
+
+// #endregion Interfaces
+
+// #region Classes
 
 @Component({
-  selector: 'app-uploader',
-  templateUrl: './uploader.component.html',
-  standalone: true,
-  imports: [
-    FormsModule,
-    SACBootstrap3FormModule,
-    SACBootstrap3ButtonModule,
-    SACBootstrap3ValidationSummaryModule,
-    SACBootstrap3UploadModule,
-  ],
+    selector: 'app-uploader',
+    templateUrl: './uploader.component.html',
+    standalone: true,
+    imports: [
+        FormsModule,
+        SACBootstrap3FormModule,
+        SACBootstrap3ButtonModule,
+        SACBootstrap3ValidationSummaryModule,
+        SACBootstrap3UploadModule,
+    ],
 })
 export class DemoUploaderComponent {
-  public values: any = {
-    fieldupload1: null,
-    fieldupload2: null,
-    fieldupload3: null,
-    fieldupload4: null,
-    fieldupload5: null,
-    fieldupload6: null,
-    fieldupload7: null,
-    fieldupload8: null,
-    fieldmultipleupload1: null,
-    fieldmultipleupload2: null,
-    fieldmultipleupload3: null,
-    fieldmultipleupload4: null,
-    dropzone1: null,
-    dropzone2: null,
-    dropzone3: null,
-    dropzone4: null,
-    dropzone5: null,
-    dropzone6: null,
-    dropzone7: null,
-  };
+    // #region Properties
 
-  @ViewChild('myForm') myForm: SacFormDirective;
+    @ViewChild('myForm') public myForm: SacFormDirective;
+    public values: any = {
+        fieldupload1: null,
+        fieldupload2: null,
+        fieldupload3: null,
+        fieldupload4: null,
+        fieldupload5: null,
+        fieldupload6: null,
+        fieldupload7: null,
+        fieldupload8: null,
+        fieldmultipleupload1: null,
+        fieldmultipleupload2: null,
+        fieldmultipleupload3: null,
+        fieldmultipleupload4: null,
+        dropzone1: null,
+        dropzone2: null,
+        dropzone3: null,
+        dropzone4: null,
+        dropzone5: null,
+        dropzone6: null,
+        dropzone7: null,
+    };
 
-  public debugAction(): void {
-    this.myForm.markAsTouched();
-    alert('Action');
-  }
+    // #endregion Properties
 
-  public showmessage(message: string): void {
-    alert(message);
-  }
+    // #region Public Methods
+
+    public debugAction(): void {
+        this.myForm.markAsTouched();
+        alert('Action');
+    }
+
+    public showmessage(message: string): void {
+        alert(message);
+    }
+
+    // #endregion Public Methods
 }
+
+// #endregion Classes

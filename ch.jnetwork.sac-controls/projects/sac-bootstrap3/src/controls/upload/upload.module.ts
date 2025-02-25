33ff98f634f5bedcd4c187058a89223ca70aa8ff
Philipp Jenni <philipp.jenni@exanic.ch>
2025-02-25 16:20:40 +0100

    feat: Add Dropzone for Boostrap3
    fix (#106)

--- ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/upload/upload.module.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/upload/upload.module.ts
@@ -1,19 +1,21 @@
+import { SACBootstrap3LayoutModule } from '../layout/layout.module';
+import { SACBootstrap3TooltipModule } from '../tooltip/tooltip.module';
+import { SacDropzoneMultipleComponent } from './dropzonemultiple';
+import { SacDropzoneSingleComponent } from './dropzonesingle';
+import { SacUploadComponent } from './upload';
+import { SacUploadMultipleComponent } from './uploadmultiple';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { SACBootstrap3LayoutModule } from '../layout/layout.module';
-import { SACBootstrap3TooltipModule } from '../tooltip/tooltip.module';
-import { SacUploadComponent } from './upload';
-import { SacUploadMultipleComponent } from './uploadmultiple';
 
 @NgModule({
-  declarations: [SacUploadComponent, SacUploadMultipleComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    SACBootstrap3LayoutModule,
-    SACBootstrap3TooltipModule,
-  ],
-  exports: [SacUploadComponent, SacUploadMultipleComponent],
+    declarations: [
+        SacUploadComponent,
+        SacUploadMultipleComponent,
+        SacDropzoneSingleComponent,
+        SacDropzoneMultipleComponent,
+    ],
+    imports: [CommonModule, FormsModule, SACBootstrap3LayoutModule, SACBootstrap3TooltipModule],
+    exports: [SacUploadComponent, SacUploadMultipleComponent, SacDropzoneSingleComponent, SacDropzoneMultipleComponent],
 })
 export class SACBootstrap3UploadModule {}

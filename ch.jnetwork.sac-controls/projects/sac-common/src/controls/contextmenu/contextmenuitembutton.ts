a1d2bde17a27b0a8c7e5a2d17c6c48fa9b82e259
Philipp Jenni <philipp.jenni@exanic.ch>
2025-02-27 22:06:06 +0100

    fix: Add missing name property to contextmenu item button

--- ch.jnetwork.sac-controls/projects/sac-common/src/controls/contextmenu/contextmenuitembutton.ts
+++ ch.jnetwork.sac-controls/projects/sac-common/src/controls/contextmenu/contextmenuitembutton.ts
@@ -1,51 +1,53 @@
+import { SacContextmenuCommon } from './contextmenu';
 import { SacContextmenuItemCommon } from './contextmenuitem';
+import { Directive, EventEmitter, Input, Output } from '@angular/core';
 
 /**
  * Base Context Menu Button Item
  */
 @Directive()
 export class SacContextmenuItemButtonCommon extends SacContextmenuItemCommon {
-  /**
-   * Constructor
-   * @param contextmenu Instance of Context Menü.
-   */
-  constructor(protected contextmenu: SacContextmenuCommon) {
-    super();
-  }
+    // #region Properties
 
-  /**
-   * Menu Item ist deaktiviert
-   */
-  protected _isdisabled: boolean = false;
+    /**
+     * Menu Item ist deaktiviert
+     */
+    protected _isdisabled: boolean = false;
 
-  /**
-   * Icon Column in Menü ist deaktiviert
-   */
-  protected _isicondisabled: boolean = false;
+    /**
+     * Icon Column in Menü ist deaktiviert
+     */
+    protected _isicondisabled: boolean = false;
 
-  /**
-   * Icon für Menü
-   */
-  @Input()
-  public icon: string;
+    /**
+     * Event wenn auf den Button geklickt wird
+     */
+    @Output()
+    public clicked: EventEmitter<void> = new EventEmitter<void>();
 
-  /**
-   * css class for button styling
-   */
-  @Input()
-  public cssclass: string = '';
+    /**
+     * css class for button styling
+     */
+    @Input()
+    public cssclass: string = '';
 
-  /**
-   * Input Property für Styling des Buttons. Definiert die Css Klassen des Buttons
-   */
-  @Input()
-  public iconstyle: string = '';
+    /**
+     * Icon für Menü
+     */
+    @Input()
+    public icon: string;
 
-  /**
-   * Image für Menü. Wird benötigt wenn das Icon für den Button ein File und nicht ein Icon Font ist.
-   */
-  @Input()
-  public image: string;
+    /**
+     * Input Property für Styling des Buttons. Definiert die Css Klassen des Buttons
+     */
+    @Input()
+    public iconstyle: string = '';
+
+    /**
+     * Image für Menü. Wird benötigt wenn das Icon für den Button ein File und nicht ein Icon Font ist.
+     */
+    @Input()
+    public image: string;
 
     /**
      * name of button. used for generate id and name
@@ -81,35 +83,40 @@ export class SacContextmenuItemButtonCommon extends SacContextmenuItemCommon {
         return this._isdisabled;
     }
 
-  /**
-   * Icon Column in Menü ist deaktiviert
-   */
-  @Input()
-  public set isicondisabled(v: boolean | string) {
-    if (v === null || v === undefined || typeof v === 'boolean') {
-      this._isicondisabled = v as boolean;
-    } else {
-      this._isicondisabled = v === 'true';
+    /**
+     * Menu Item ist deaktiviert
+     */
+    @Input()
+    public set isdisabled(v: boolean | string) {
+        if (v === null || v === undefined || typeof v === 'boolean') {
+            this._isdisabled = v as boolean;
+        } else {
+            this._isdisabled = v === 'true';
+        }
     }
-  }
-  /**
-   * Icon Column in Menü ist deaktiviert
-   */
-  public get isicondisabled(): boolean | string {
-    return this._isicondisabled;
-  }
 
-  /**
-   * Text für Menü Item.
-   */
-  @Input()
-  public text: string;
+    /**
+     * Icon Column in Menü ist deaktiviert
+     */
+    public get isicondisabled(): boolean | string {
+        return this._isicondisabled;
+    }
 
-  /**
-   * Event wenn auf den Button geklickt wird
-   */
-  @Output()
-  clicked: EventEmitter<void> = new EventEmitter<void>();
+    /**
+     * Icon Column in Menü ist deaktiviert
+     */
+    @Input()
+    public set isicondisabled(v: boolean | string) {
+        if (v === null || v === undefined || typeof v === 'boolean') {
+            this._isicondisabled = v as boolean;
+        } else {
+            this._isicondisabled = v === 'true';
+        }
+    }
+
+    // #endregion Public Getters And Setters
+
+    // #region Public Methods
 
     /**
      * Die Methode wird das cklickaction Emitter aktivieren

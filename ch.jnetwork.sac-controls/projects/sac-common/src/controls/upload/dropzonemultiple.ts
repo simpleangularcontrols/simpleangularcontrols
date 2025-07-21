89615aed469cbb401c636030d123daf1fa80fb7e
Philipp Jenni <philipp.jenni@gmx.ch>
2025-07-21 18:27:20 +0200

    fix (#300): Buttonupload and Buttonbrowse cannot be used during upload (#307)
    * fix (#300): Buttonupload and Buttonbrowse cannot be used during upload
    * fix: solved layout issue in bs4/bs5 multiple uploader

--- ch.jnetwork.sac-controls/projects/sac-common/src/controls/upload/dropzonemultiple.ts
+++ ch.jnetwork.sac-controls/projects/sac-common/src/controls/upload/dropzonemultiple.ts
@@ -1,187 +1,182 @@
+import { SacUploadBase } from '../../common/baseuploadcontrol';
+import { Validation } from '../../validation';
 import { Directive, Input, OnInit } from '@angular/core';
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 import { UploadState } from 'ngx-uploadx';
-import { SacUploadBase } from '../../common/baseuploadcontrol';
-import { Validation } from '../../validation';
 
 /**
  * Upload Komponente für ein einzelnes File
  */
 @Directive()
-export class SacDropzoneMultipleCommon
-  extends SacUploadBase<string[]>
-  implements OnInit
-{
-  // #region Properties
+export class SacDropzoneMultipleCommon extends SacUploadBase<string[]> implements OnInit {
+    // #region Properties
 
-  /**
-   * Max. Anzahl Files die hochgeladen werden können
-   */
-  @Input()
-  public maxfiles: number = 0;
-  /**
-   * Min. Anzahl Files die hochgeladen werden müssen
-   */
-  @Input()
-  public minfiles: number = 0;
-  /**
-   * Höhe des Upload Controls
-   */
-  @Input()
-  public uploadheight: string = null;
-  /**
-   * Resource Key für Validation Message Required bei Control
-   */
-  @Input() public validationmessageminfiles: string =
-    this.validationKeyService.ValidationErrorFilesMin;
-  /**
-   * Resource Key für Validation Message Required in Validation Summary
-   */
-  @Input()
-  public validationmessagesummaryminfiles: string =
-    this.validationKeyService.ValidationErrorSummaryFilesMin;
+    /**
+     * Property wenn Drag Event aktiv ist (Maus über Zone)
+     */
+    public active = false;
 
-  /**
-   * Property wenn Drag Event aktiv ist (Maus über Zone)
-   */
-  public active = false;
+    /**
+     * Max. Anzahl Files die hochgeladen werden können
+     */
+    @Input()
+    public maxfiles: number = 0;
 
-  // #endregion Properties
+    /**
+     * Min. Anzahl Files die hochgeladen werden müssen
+     */
+    @Input()
+    public minfiles: number = 0;
 
-  // #region Public Methods
+    /**
+     * Höhe des Upload Controls
+     */
+    @Input()
+    public uploadheight: string = null;
 
-  /**
-   * Prüft ob die max. Files in der Queue nicht überschritten werden
-   *
-   * @param file File das hinzugefügt wurde
-   */
-  public CustomAddValidation(file: UploadState): boolean {
-    if (this.maxfiles > 0 && this.uploads.length >= this.maxfiles) {
-      this.onfileerror.emit('INVALID_MAXFILES');
-      return false;
-    } else {
-      return true;
-    }
-  }
+    /**
+     * Resource Key für Validation Message Required bei Control
+     */
+    @Input() public validationmessageminfiles: string = this.validationKeyService.ValidationErrorFilesMin;
 
-  /**
-   * Setzt die File ID's der hochgeladen Files in das Model
-   *
-   * @param file ID des Files welches hochgeladen wurde.
-   */
-  public SetUploadValue(file: UploadState) {
-    let documentid: string = null;
-    if (file === null) {
-      documentid = null;
-    } else {
-      if (
-        file.response !== undefined &&
-        file.response !== null &&
-        file.response.documentid !== null &&
-        file.response.documentid !== undefined
-      ) {
-        documentid = file.response.documentid;
-      } else {
-        documentid = file.uploadId;
-      }
+    /**
+     * Resource Key für Validation Message Required in Validation Summary
+     */
+    @Input()
+    public validationmessagesummaryminfiles: string = this.validationKeyService.ValidationErrorSummaryFilesMin;
 
-      // Document ID aktualisieren, damit Wert von Server in Model gesetzt werden kann.
-      this.uploads
-        .filter((itm) => itm !== null && itm.uploadId === file.uploadId)
-        .forEach((itm) => {
-          itm.documentid = documentid;
-        });
-    }
+    // #endregion Properties
 
-    // List of Files
-    const fileIds: string[] = [];
+    // #region Public Methods
 
-    // Add all Items with Uploaded State to Model
-    this.uploads
-      .filter((itm) => itm.status === 'complete')
-      .forEach((itm) => {
-        if (itm.documentid !== null && itm.documentid !== undefined) {
-          fileIds.push(itm.documentid);
+    /**
+     * Prüft ob die max. Files in der Queue nicht überschritten werden
+     *
+     * @param file File das hinzugefügt wurde
+     */
+    public CustomAddValidation(file: UploadState): boolean {
+        if (this.maxfiles > 0 && this.uploads.length >= this.maxfiles) {
+            this.onfileerror.emit('INVALID_MAXFILES');
+            return false;
+        } else {
+            return true;
         }
-      });
-
-    if (fileIds.length > 0) {
-      super.setValue(fileIds);
-    } else {
-      super.setValue(null);
-    }
-  }
-
-  /**
-   * Methode für Drag and Drop von Files
-   * @param event Drag Event
-   */
-  public dropHandler(event: DragEvent): void {
-    if (
-      event.dataTransfer &&
-      event.dataTransfer.files &&
-      event.dataTransfer.files.item(0)
-    ) {
-      event.stopPropagation();
-      event.preventDefault();
-      this.active = false;
-      this.uploadService.handleFileList(event.dataTransfer.files);
-    }
-  }
-
-  /**
-   * Ervent wenn das Control initialisert wird
-   */
-  public ngOnInit() {
-    super.ngOnInit();
-
-    this.autoupload = true;
-  }
-
-  /**
-   * Methode wenn Drag die Zone verlässt
-   * @param event DragLeave Event
-   */
-  public onDragLeave(event: DragEvent): void {
-    this.active = false;
-  }
-
-  /**
-   * Methode wenn Drag in die Zone eintritt
-   * @param event DragEnter Event
-   */
-  public onDragOver(event: DragEvent): void {
-    if (
-      event.dataTransfer &&
-      event.dataTransfer.files &&
-      event.dataTransfer.types.every((itm) => itm === 'Files') &&
-      event.dataTransfer.types.length > 0
-    ) {
-      event.dataTransfer.dropEffect = 'copy';
-      event.stopPropagation();
-      event.preventDefault();
-      this.active = true;
-    }
-  }
-
-  /**
-   * Validiert das Control
-   *
-   * @param c Control
-   */
-  public validateData(c: AbstractControl): ValidationErrors | null {
-    let error: ValidationErrors | null = super.validateData(c);
-
-    if (error === null) {
-      error = Validation.minFiles(
-        this.minfiles,
-        this.validationmessageminfiles,
-        this.validationmessagesummaryminfiles
-      )(c);
     }
 
-    return error;
-  }
+    /**
+     * Setzt die File ID's der hochgeladen Files in das Model
+     *
+     * @param file ID des Files welches hochgeladen wurde.
+     */
+    public SetUploadValue(file: UploadState) {
+        let documentid: string = null;
+        if (file === null) {
+            documentid = null;
+        } else {
+            if (
+                file.response !== undefined &&
+                file.response !== null &&
+                file.response.documentid !== null &&
+                file.response.documentid !== undefined
+            ) {
+                documentid = file.response.documentid;
+            } else {
+                documentid = file.uploadId;
+            }
 
-  // #endregion Public Methods
+            // Document ID aktualisieren, damit Wert von Server in Model gesetzt werden kann.
+            this.uploads
+                .filter((itm) => itm !== null && itm.uploadId === file.uploadId)
+                .forEach((itm) => {
+                    itm.documentid = documentid;
+                });
+        }
+
+        // List of Files
+        const fileIds: string[] = [];
+
+        // Add all Items with Uploaded State to Model
+        this.uploads
+            .filter((itm) => itm.status === 'complete')
+            .forEach((itm) => {
+                if (itm.documentid !== null && itm.documentid !== undefined) {
+                    fileIds.push(itm.documentid);
+                }
+            });
+
+        if (fileIds.length > 0) {
+            super.setValue(fileIds);
+        } else {
+            super.setValue(null);
+        }
+    }
+
+    /**
+     * Methode für Drag and Drop von Files
+     * @param event Drag Event
+     */
+    public dropHandler(event: DragEvent): void {
+        if (event.dataTransfer && event.dataTransfer.files && event.dataTransfer.files.item(0)) {
+            event.stopPropagation();
+            event.preventDefault();
+            this.active = false;
+            this.uploadService.handleFileList(event.dataTransfer.files);
+        }
+    }
+
+    /**
+     * Ervent wenn das Control initialisert wird
+     */
+    public ngOnInit() {
+        super.ngOnInit();
+
+        this.autoupload = true;
+    }
+
+    /**
+     * Methode wenn Drag die Zone verlässt
+     * @param event DragLeave Event
+     */
+    public onDragLeave(event: DragEvent): void {
+        this.active = false;
+    }
+
+    /**
+     * Methode wenn Drag in die Zone eintritt
+     * @param event DragEnter Event
+     */
+    public onDragOver(event: DragEvent): void {
+        if (
+            event.dataTransfer &&
+            event.dataTransfer.files &&
+            event.dataTransfer.types.every((itm) => itm === 'Files') &&
+            event.dataTransfer.types.length > 0
+        ) {
+            event.dataTransfer.dropEffect = 'copy';
+            event.stopPropagation();
+            event.preventDefault();
+            this.active = true;
+        }
+    }
+
+    /**
+     * Validiert das Control
+     *
+     * @param c Control
+     */
+    public validateData(c: AbstractControl): ValidationErrors | null {
+        let error: ValidationErrors | null = super.validateData(c);
+
+        if (error === null) {
+            error = Validation.minFiles(
+                this.minfiles,
+                this.validationmessageminfiles,
+                this.validationmessagesummaryminfiles
+            )(c);
+        }
+
+        return error;
+    }
+
+    // #endregion Public Methods
 }

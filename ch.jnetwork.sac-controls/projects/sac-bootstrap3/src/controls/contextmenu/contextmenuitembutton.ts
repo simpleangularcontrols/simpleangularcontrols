ebe6c390b26fe9c63d8edd60d3c5215a7e2d0d57
Philipp Jenni <philipp.jenni@exanic.ch>
2025-08-26 15:23:33 +0200

    fix: Hide Destroyed ContextMenu Items

--- ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/contextmenu/contextmenuitembutton.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/contextmenu/contextmenuitembutton.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, forwardRef } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, forwardRef } from '@angular/core';
 import { SacContextmenuItemButtonCommon, SacContextmenuItemCommon } from '@simpleangularcontrols/sac-common';
 
 /**
@@ -14,7 +14,13 @@ import { SacContextmenuItemButtonCommon, SacContextmenuItemCommon } from '@simpl
         },
     ],
 })
-export class SacContextmenuItemButtonComponent extends SacContextmenuItemButtonCommon implements OnInit {
+export class SacContextmenuItemButtonComponent extends SacContextmenuItemButtonCommon implements OnInit, OnDestroy {
+    // #region Properties
+
+    public ref: ChildNode[] = [];
+
+    // #endregion Properties
+
     // #region Constructors
 
     /**
@@ -29,14 +35,19 @@ export class SacContextmenuItemButtonComponent extends SacContextmenuItemButtonC
 
     // #region Public Methods
 
+    public ngOnDestroy(): void {
+        for (let i = 0; i < this.ref.length; i++) {
+            this.ref[i].parentElement.removeChild(this.ref[i]);
+        }
+    }
+
     public ngOnInit(): void {
         const rootElement: HTMLElement = this.el.nativeElement;
         const parentElement: HTMLElement = rootElement.parentElement;
-
         while (rootElement.firstChild) {
+            this.ref.push(rootElement.firstChild);
             parentElement.insertBefore(rootElement.firstChild, rootElement);
         }
-
         parentElement.removeChild(rootElement);
     }
 

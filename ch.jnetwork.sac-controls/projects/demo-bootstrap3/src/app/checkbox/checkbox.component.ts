0ede79d311fdf699416701e2d50d682a2dff990f
Philipp Jenni <philipp.jenni@gmx.ch>
2025-09-01 15:34:32 +0200

    fix (#331): Binding with numeric values in RadioButtons (#332)

--- ch.jnetwork.sac-controls/projects/demo-bootstrap3/src/app/checkbox/checkbox.component.ts
+++ ch.jnetwork.sac-controls/projects/demo-bootstrap3/src/app/checkbox/checkbox.component.ts
@@ -1,42 +1,52 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {
-  SACBootstrap3ButtonModule,
-  SACBootstrap3CheckboxModule,
-  SACBootstrap3FormModule,
-  SACBootstrap3StaticLabelModule,
-  SACBootstrap3ValidationSummaryModule,
-  SacFormDirective,
+    SACBootstrap3ButtonModule,
+    SACBootstrap3CheckboxModule,
+    SACBootstrap3FormModule,
+    SACBootstrap3StaticLabelModule,
+    SACBootstrap3ValidationSummaryModule,
+    SacFormDirective,
 } from '@simpleangularcontrols/sac-bootstrap3';
 
 @Component({
-  selector: 'app-checkbox',
-  templateUrl: './checkbox.component.html',
-  standalone: true,
-  imports: [
-    FormsModule,
-    SACBootstrap3FormModule,
-    SACBootstrap3ButtonModule,
-    SACBootstrap3ValidationSummaryModule,
-    SACBootstrap3CheckboxModule,
-    SACBootstrap3StaticLabelModule,
-  ],
+    selector: 'app-checkbox',
+    templateUrl: './checkbox.component.html',
+    standalone: true,
+    imports: [
+        FormsModule,
+        SACBootstrap3FormModule,
+        SACBootstrap3ButtonModule,
+        SACBootstrap3ValidationSummaryModule,
+        SACBootstrap3CheckboxModule,
+        SACBootstrap3StaticLabelModule,
+    ],
 })
 export class DemoCheckboxComponent {
-  public values: any = {
-    checkbox1: false,
-    checkbox2: false,
-    checkbox3: false,
-    checkbox4: false,
-    checkbox5: false,
-    radiobuttons1: '1',
-    radiobuttons2: '2',
-  };
+    // #region Properties
 
-  @ViewChild('myForm') myForm: SacFormDirective;
+    @ViewChild('myForm') public myForm: SacFormDirective;
+    public values: any = {
+        checkbox1: false,
+        checkbox2: false,
+        checkbox3: false,
+        checkbox4: false,
+        checkbox5: false,
+        radiobuttons1: '1',
+        radiobuttons2: null,
+        radiobuttons3: 2,
+        radiobuttons4: '2',
+        radiobuttons5: '2',
+    };
 
-  public debugAction(): void {
-    this.myForm.markAsTouched();
-    alert('Action');
-  }
+    // #endregion Properties
+
+    // #region Public Methods
+
+    public debugAction(): void {
+        this.myForm.markAsTouched();
+        alert('Action');
+    }
+
+    // #endregion Public Methods
 }

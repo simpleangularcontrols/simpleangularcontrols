da2dfda569036bac99a04d083d0e79f6a2829581
Philipp Jenni <philipp.jenni@gmx.ch>
2025-09-03 11:04:28 +0200

    fix (#331): Binding with numeric values in RadioButtons (#334)

--- ch.jnetwork.sac-controls/projects/sac-common/src/controls/checkbox/radiobuttons.ts
+++ ch.jnetwork.sac-controls/projects/sac-common/src/controls/checkbox/radiobuttons.ts
@@ -1,134 +1,131 @@
-import { Directive, Host, Injector, Input } from '@angular/core';
-import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
 import { SacBaseModelControl } from '../../common/basemodelcontrol';
 import { Validation } from '../../validation';
 import { SacFormLayoutCommon } from '../layout/formlayout';
 import { SacRadiobuttonCommon } from './radiobutton';
+import { Directive, Host, Injector, Input } from '@angular/core';
+import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
 
 /**
  * Basis Komponente für SacRadiobuttonsCommon. Extends SacBaseModelControl
  */
 @Directive()
-export abstract class SacRadiobuttonsCommon
-  extends SacBaseModelControl<any>
-  implements Validator
-{
-  // #region Properties
+export abstract class SacRadiobuttonsCommon extends SacBaseModelControl<any> implements Validator {
+    // #region Properties
 
-  /**
-   * Radio Buttons Content
-   */
-  private contentRadiobuttons: SacRadiobuttonCommon[] = [];
-  /**
-   * Radio Button Index
-   */
-  private radioButtonIndex: number = 0;
+    /**
+     * Radio Buttons Content
+     */
+    private contentRadiobuttons: SacRadiobuttonCommon[] = [];
 
-  /**
-   * Resource Key für Validation Message Required bei Control
-   */
-  @Input() public validationmessagerequired: string =
-    this.validationKeyService.ValidationErrorRequired;
-  /**
-   * Resource Key für Validation Message Required in Validation Summary
-   */
-  @Input() public validationmessagesummaryrequired: string =
-    this.validationKeyService.ValidationErrorSummaryRequired;
+    /**
+     * Radio Button Index
+     */
+    private radioButtonIndex: number = 0;
 
-  // #endregion Properties
+    /**
+     * Resource Key für Validation Message Required bei Control
+     */
+    @Input() public validationmessagerequired: string = this.validationKeyService.ValidationErrorRequired;
 
-  // #region Constructors
+    /**
+     * Resource Key für Validation Message Required in Validation Summary
+     */
+    @Input() public validationmessagesummaryrequired: string = this.validationKeyService.ValidationErrorSummaryRequired;
 
-  /**
-   * Constructor
-   * @param formlayout SacFormLayoutCommon to define scoped layout settings
-   * @param injector Injector for injecting services
-   */
-  constructor(@Host() formlayout: SacFormLayoutCommon, injector: Injector) {
-    super(formlayout, injector);
-  }
+    // #endregion Properties
 
-  // #endregion Constructors
+    // #region Constructors
 
-  // #region Public Methods
-
-  /**
-   * GEtter für Radio Button Index
-   */
-  public GetRadionButtonIndex(): number {
-    this.radioButtonIndex++;
-    return this.radioButtonIndex;
-  }
-
-  /**
-   * Methode prüft ob Item checked ist
-   */
-  public HasCheckedItem(): boolean {
-    const radioButtons: SacRadiobuttonCommon[] = this.contentRadiobuttons;
-
-    if (radioButtons === undefined || radioButtons === null) {
-      return false;
+    /**
+     * Constructor
+     * @param formlayout SacFormLayoutCommon to define scoped layout settings
+     * @param injector Injector for injecting services
+     */
+    constructor(@Host() formlayout: SacFormLayoutCommon, injector: Injector) {
+        super(formlayout, injector);
     }
 
-    return this.contentRadiobuttons.some((itm) => itm.checked);
-  }
+    // #endregion Constructors
 
-  /**
-   * Erstellung des RadioButton
-   */
-  public RegisterRadioButton(radioButton: SacRadiobuttonCommon) {
-    this.contentRadiobuttons.push(radioButton);
-  }
+    // #region Public Methods
 
-  /**
-   * Item selektieren
-   */
-  public SelectItem(value: any) {
-    this.contentRadiobuttons.forEach((itm) => {
-      itm.checked = itm.value === value;
-    });
-
-    this.value = value;
-  }
-
-  /**
-   * Löschen des Radio Button
-   */
-  public UnregisterRadioButton(radioButton: SacRadiobuttonCommon) {
-    const index: number = this.contentRadiobuttons.indexOf(radioButton);
-
-    if (index >= 0) {
-      this.contentRadiobuttons.splice(index, 1);
+    /**
+     * Getter für Radio Button Index
+     */
+    public GetRadionButtonIndex(): number {
+        this.radioButtonIndex++;
+        return this.radioButtonIndex;
     }
-  }
 
-  /**
-   * Validator
-   */
-  public validateData(c: AbstractControl): ValidationErrors {
-    if (!this.HasCheckedItem()) {
-      return Validation.GetValidationErrorItem(
-        'required',
-        this.validationmessagerequired,
-        this.validationmessagesummaryrequired,
-        this.label
-      );
-    } else {
-      return null;
+    /**
+     * Methode prüft ob Item checked ist
+     */
+    public HasCheckedItem(): boolean {
+        const radioButtons: SacRadiobuttonCommon[] = this.contentRadiobuttons;
+
+        if (radioButtons === undefined || radioButtons === null) {
+            return false;
+        }
+
+        return this.contentRadiobuttons.some((itm) => itm.checked);
     }
-  }
 
-  /**
-   * Wert schreiben
-   */
-  public writeValue(value: any) {
-    super.writeValue(value);
-    if (value !== null && value !== undefined) {
-      this.contentRadiobuttons.forEach((itm) => {
-        itm.checked = itm.value === value;
-      });
+    /**
+     * Erstellung des RadioButton
+     */
+    public RegisterRadioButton(radioButton: SacRadiobuttonCommon) {
+        this.contentRadiobuttons.push(radioButton);
     }
-  }
 
-  // #endregion Public Methods
+    /**
+     * Item selektieren
+     */
+    public SelectItem(value: any) {
+        this.contentRadiobuttons.forEach((itm) => {
+            itm.checked = itm.value === value;
+        });
+
+        this.value = value;
+    }
+
+    /**
+     * Löschen des Radio Button
+     */
+    public UnregisterRadioButton(radioButton: SacRadiobuttonCommon) {
+        const index: number = this.contentRadiobuttons.indexOf(radioButton);
+
+        if (index >= 0) {
+            this.contentRadiobuttons.splice(index, 1);
+        }
+    }
+
+    /**
+     * Validator
+     */
+    public validateData(c: AbstractControl): ValidationErrors {
+        if (!this.HasCheckedItem()) {
+            return Validation.GetValidationErrorItem(
+                'required',
+                this.validationmessagerequired,
+                this.validationmessagesummaryrequired,
+                this.label
+            );
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Wert schreiben
+     */
+    public writeValue(value: any) {
+        super.writeValue(value);
+        if (value !== null && value !== undefined) {
+            this.contentRadiobuttons.forEach((itm) => {
+                itm.checked = itm.value === value;
+            });
+        }
+    }
+
+    // #endregion Public Methods
 }

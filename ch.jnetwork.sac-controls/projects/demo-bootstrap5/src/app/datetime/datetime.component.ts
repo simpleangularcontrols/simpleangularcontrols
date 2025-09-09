61eebb811110c8e84e02a2017c0e5cc6d9805195
Philipp Jenni <philipp.jenni@gmx.ch>
2025-09-09 21:30:39 +0200

    fix: Popup in Picker not closing if component hides  (#341)
    * fix (#339): Popup in Picker not closing if component hides
    * fix: Optimize DateTime Dialog in BS3 for Mobile
    * fix: Add Missing NgIf in Demo Standalone Components

--- ch.jnetwork.sac-controls/projects/demo-bootstrap5/src/app/datetime/datetime.component.ts
+++ ch.jnetwork.sac-controls/projects/demo-bootstrap5/src/app/datetime/datetime.component.ts
@@ -1,6 +1,13 @@
+import { NgIf } from '@angular/common';
 import { Component, ViewChild } from '@angular/core';
-import { SacFormDirective, SACBootstrap5FormModule, SACBootstrap5ButtonModule, SACBootstrap5ValidationSummaryModule, SACBootstrap5DateTimeModule } from '@simpleangularcontrols/sac-bootstrap5';
 import { FormsModule } from '@angular/forms';
+import {
+    SACBootstrap5ButtonModule,
+    SACBootstrap5DateTimeModule,
+    SACBootstrap5FormModule,
+    SACBootstrap5ValidationSummaryModule,
+    SacFormDirective,
+} from '@simpleangularcontrols/sac-bootstrap5';
 
 @Component({
     selector: 'app-datetime',
@@ -12,41 +19,47 @@ import { FormsModule } from '@angular/forms';
         SACBootstrap5ButtonModule,
         SACBootstrap5ValidationSummaryModule,
         SACBootstrap5DateTimeModule,
+        NgIf,
     ],
 })
 export class DemoDatetimeComponent {
-  // #region Properties
+    // #region Properties
 
-  @ViewChild('myForm') public myForm: SacFormDirective;
+    @ViewChild('myForm') public myForm: SacFormDirective;
+    public values: any = {
+        datum1: '',
+        datum2: new Date(Date.now()),
+        datum3: '',
+        datum4: '',
+        datum5: '',
+        time1: new Date(Date.now()),
+        time2: '',
+        time3: '',
+        time4: '',
+        time5: new Date(Date.now()),
+        time6: '',
+        datumzeit1: new Date(Date.now()),
+        datumzeit2: '2024-11-22T17:10:00+01:00',
+        datumzeit3: '',
+        datumzeit4: '',
+        datumzeit5: '',
+        datumzeit6: '2024-11-18T07:54:28.870Z',
+        datumzeit7: '2024-11-18T07:54:28.870Z',
+        datumzeit7visible: true,
+    };
 
-  public values: any = {
-    datum1: '',
-    datum2: new Date(Date.now()),
-    datum3: '',
-    datum4: '',
-    datum5: '',
-    time1: new Date(Date.now()),
-    time2: '',
-    time3: '',
-    time4: '',
-    time5: new Date(Date.now()),
-    time6: '',
-    datumzeit1: new Date(Date.now()),
-    datumzeit2: '2024-11-22T17:10:00+01:00',
-    datumzeit3: '',
-    datumzeit4: '',
-    datumzeit5: '',
-    datumzeit6: '2024-11-18T07:54:28.870Z',
-  };
+    // #endregion Properties
 
-  // #endregion Properties
+    // #region Public Methods
 
-  // #region Public Methods
+    public debugAction(): void {
+        this.myForm.markAsTouched();
+        alert('Action');
+    }
 
-  public debugAction(): void {
-    this.myForm.markAsTouched();
-    alert('Action');
-  }
+    public hideDatumzeit7(): void {
+        this.values.datumzeit7visible = false;
+    }
 
-  // #endregion Public Methods
+    // #endregion Public Methods
 }

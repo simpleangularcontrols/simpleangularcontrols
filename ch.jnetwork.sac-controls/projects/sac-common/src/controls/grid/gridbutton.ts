9230489451988a0879e2de71e90bcc6435544bb9
Philipp Jenni <philipp.jenni@gmx.ch>
2025-12-16 18:46:27 +0100

    feat (#299): Add Data E2E Attribute for GridButton (#357)

--- ch.jnetwork.sac-controls/projects/sac-common/src/controls/grid/gridbutton.ts
+++ ch.jnetwork.sac-controls/projects/sac-common/src/controls/grid/gridbutton.ts
@@ -5,63 +5,71 @@ import { Directive, EventEmitter, Input, Output } from '@angular/core';
  */
 @Directive()
 export class SacGridButtonCommon {
-  // #region Properties
+    // #region Properties
 
-  /**
-   * Button is deactivated
-   */
-  protected _isdisabledvalue: boolean = false;
+    /**
+     * Button is deactivated
+     */
+    protected _isdisabledvalue: boolean = false;
 
-  /**
-   * Icon name from sprite
-   */
-  @Input()
-  public icon: string;
-  /**
-   * Input property for styling the button. Defines the CSS classes of the button
-   */
-  @Input()
-  public iconstyle: string = '';
-  /**
-   * Event when the button is clicked
-   */
-  @Output()
-  public clicked: EventEmitter<any> = new EventEmitter<any>();
+    /**
+     * Event when the button is clicked
+     */
+    @Output()
+    public clicked: EventEmitter<any> = new EventEmitter<any>();
 
-  // #endregion Properties
+    /**
+     * Icon name from sprite
+     */
+    @Input()
+    public icon: string;
 
-  // #region Public Getters And Setters
+    /**
+     * Input property for styling the button. Defines the CSS classes of the button
+     */
+    @Input()
+    public iconstyle: string = '';
 
-  /**
-   * Deactivating buttons
-   * @param v Deactivates the button. Can be a Boolean or the strings `true` or `false`.
-   * @return Defines whether the button is deactivated. Is always a Boolean type
-   */
-  @Input()
-  public set isdisabled(v: boolean | string) {
-    if (v === null || v === undefined || typeof v === 'boolean') {
-      this._isdisabledvalue = v as boolean;
-    } else {
-      this._isdisabledvalue = v === 'true';
+    /**
+     * Name of Button inside Grid
+     */
+    @Input()
+    public name: string;
+
+    // #endregion Properties
+
+    // #region Public Getters And Setters
+
+    public get isdisabled(): boolean | string {
+        return this._isdisabledvalue;
     }
-  }
 
-  public get isdisabled(): boolean | string {
-    return this._isdisabledvalue;
-  }
-
-  // #endregion Public Getters And Setters
-
-  // #region Public Methods
-
-  /**
-   * Triggers the clicked event if the button is not deactivated.
-   */
-  public callaction() {
-    if (!this._isdisabledvalue) {
-      this.clicked.emit(this.iconstyle);
+    /**
+     * Deactivating buttons
+     * @param v Deactivates the button. Can be a Boolean or the strings `true` or `false`.
+     * @return Defines whether the button is deactivated. Is always a Boolean type
+     */
+    @Input()
+    public set isdisabled(v: boolean | string) {
+        if (v === null || v === undefined || typeof v === 'boolean') {
+            this._isdisabledvalue = v as boolean;
+        } else {
+            this._isdisabledvalue = v === 'true';
+        }
     }
-  }
 
-  // #endregion Public Methods
+    // #endregion Public Getters And Setters
+
+    // #region Public Methods
+
+    /**
+     * Triggers the clicked event if the button is not deactivated.
+     */
+    public callaction() {
+        if (!this._isdisabledvalue) {
+            this.clicked.emit(this.iconstyle);
+        }
+    }
+
+    // #endregion Public Methods
 }

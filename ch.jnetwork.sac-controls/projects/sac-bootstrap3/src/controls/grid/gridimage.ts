3cd3fe19d71b19f47276631a2dba9e9f46f7673d
Philipp Jenni <philipp.jenni@exanic.ch>
2025-12-17 20:02:01 +0100

    feat (#297): Allow alignment of grid columns

--- ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/grid/gridimage.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/grid/gridimage.ts
@@ -1,22 +1,8 @@
-import { Component, Input, Output, EventEmitter, ElementRef } from '@angular/core';
-import { SacGridCommon, SacGridImageCommon } from '@simpleangularcontrols/sac-common';
-
+import { Component } from '@angular/core';
+import { SacGridImageCommon } from '@simpleangularcontrols/sac-common';
 
 @Component({
-  selector: 'sac-gridimage',
-  templateUrl: './gridimage.html'
+    selector: 'sac-gridimage',
+    templateUrl: './gridimage.html',
 })
-export class SacGridImageComponent extends SacGridImageCommon {
-
-  public getIconClass(): string {
-    switch (this.iconstyle) {
-      case 'edit':
-        return 'sprite icon-sprite-base-main_edit';
-      case 'delete':
-        return 'sprite icon-sprite-base-main_delete';
-      default:
-        return this.iconstyle;
-    }
-  }
-
-}
+export class SacGridImageComponent extends SacGridImageCommon {}

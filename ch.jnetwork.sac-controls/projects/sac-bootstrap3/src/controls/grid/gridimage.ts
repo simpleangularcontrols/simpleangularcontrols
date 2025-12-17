60f7d99e983d5bffccdaba93d0ea4cf7f04415da
Philipp Jenni <philipp.jenni@exanic.ch>
2025-12-17 20:35:58 +0100

    fix: add missing imports in gridimage

--- ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/grid/gridimage.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/grid/gridimage.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SacGridImageCommon } from '@simpleangularcontrols/sac-common';
 
 @Component({
     selector: 'sac-gridimage',
     templateUrl: './gridimage.html',
+    imports: [CommonModule],
     standalone: true,
 })
 export class SacGridImageComponent extends SacGridImageCommon {}
--- ch.jnetwork.sac-controls/projects/sac-bootstrap5/src/controls/grid/gridimage.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap5/src/controls/grid/gridimage.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { SacGridImageCommon } from '@simpleangularcontrols/sac-common';
 
@@ -7,6 +8,7 @@ import { SacGridImageCommon } from '@simpleangularcontrols/sac-common';
 @Component({
     selector: 'sac-gridimage',
     templateUrl: './gridimage.html',
+    imports: [CommonModule],
     standalone: true,
 })
 export class SacGridImageComponent extends SacGridImageCommon {}

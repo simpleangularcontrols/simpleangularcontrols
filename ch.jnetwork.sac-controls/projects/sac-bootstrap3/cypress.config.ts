99b71b9b45d02cbc4ca6bf71a396281d88689ff3
philippjenni <philipp.jenni@gmx.ch>
2025-12-23 18:20:10 +0100

    feat: extend coverage report for any bs version

--- ch.jnetwork.sac-controls/projects/sac-bootstrap3/cypress.config.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap3/cypress.config.ts
@@ -2,41 +2,47 @@ import { defineConfig } from 'cypress';
 import coverageWebpack from 'projects/sac-bootstrap3/cypress/coverage.webpack';
 
 export default defineConfig({
-  viewportWidth: 1024,
-  viewportHeight: 768,
-  reporter: './node_modules/mochawesome/src/mochawesome.js',
-  reporterOptions: {
-    reportDir: 'reports/bs3',
-    overwrite: false,
-    html: false,
-    json: true,
-  },
+    viewportWidth: 1024,
+    viewportHeight: 768,
+    reporter: './node_modules/mochawesome/src/mochawesome.js',
+    reporterOptions: {
+        reportDir: 'reports/bs3',
+        overwrite: false,
+        html: false,
+        json: true,
+    },
 
-  component: {
-    supportFile: 'projects/sac-bootstrap3/cypress/support/component.ts',
-    supportFolder: 'projects/sac-bootstrap3/cypress/support',
-    indexHtmlFile:
-      'projects/sac-bootstrap3/cypress/support/component-index.html',
-    devServer: {
-      framework: 'angular',
-      bundler: 'webpack',
-      webpackConfig: coverageWebpack,
-      options: {
-        projectConfig: {
-          root: 'projects/sac-bootstrap3',
-          sourceRoot: 'projects/sac-bootstrap3/src',
-          buildOptions: {
-            outputPath: 'dist/sac-bootstrap3',
-            main: 'src/entrypoint-cypress.ts',
-            tsConfig: 'projects/sac-bootstrap3/tsconfig.lib.json',
-          },
+    component: {
+        supportFile: 'projects/sac-bootstrap3/cypress/support/component.ts',
+        supportFolder: 'projects/sac-bootstrap3/cypress/support',
+        indexHtmlFile: 'projects/sac-bootstrap3/cypress/support/component-index.html',
+        devServer: {
+            framework: 'angular',
+            bundler: 'webpack',
+            webpackConfig: coverageWebpack,
+            options: {
+                projectConfig: {
+                    root: 'projects/sac-bootstrap3',
+                    sourceRoot: 'projects/sac-bootstrap3/src',
+                    buildOptions: {
+                        outputPath: 'dist/sac-bootstrap3',
+                        main: 'src/entrypoint-cypress.ts',
+                        tsConfig: 'projects/sac-bootstrap3/tsconfig.lib.json',
+                        assets: [
+                            {
+                                glob: '**/*',
+                                input: 'node_modules/tinymce',
+                                output: 'tinymce/',
+                            },
+                        ],
+                    },
+                },
+            },
         },
-      },
+        specPattern: 'projects/sac-bootstrap3/src/**/*.cy.ts',
+        setupNodeEvents(on, config) {
+            require('@cypress/code-coverage/task')(on, config);
+            return config;
+        },
     },
-    specPattern: 'projects/sac-bootstrap3/src/**/*.cy.ts',
-    setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config);
-      return config;
-    },
-  },
 });

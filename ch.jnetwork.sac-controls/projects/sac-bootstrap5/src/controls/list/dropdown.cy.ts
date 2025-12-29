42f6ce4b2c4717c6c4f824c6961ecd43c6c1525f
philippjenni <philipp.jenni@gmx.ch>
2025-12-29 21:15:25 +0100

    feat: add tests to dropdown and listbox control

--- ch.jnetwork.sac-controls/projects/sac-bootstrap5/src/controls/list/dropdown.cy.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap5/src/controls/list/dropdown.cy.ts
@@ -7,7 +7,16 @@ describe('SacDropdownComponent', () => {
     it('should show label and component', () => {
         cy.mount(
             `<form>
-                <sac-dropdown name="dropdown" [label]="label" [emptyvalue]="null" emptylabel="Please select" [ngModel]="null"></sac-dropdown>
+                <sac-dropdown 
+                    name="dropdown" 
+                    [label]="label"
+                    [emptyvalue]="null" 
+                    emptylabel="Please select"
+                    [ngModel]="null">
+                    <option [ngValue]="0">Value Item 1</option>
+                    <option [ngValue]="1">Value Item 2</option>
+                    <option [ngValue]="2">Value Item 3</option>                    
+                </sac-dropdown>
             </form>`,
             {
                 imports: [FormsModule, SacFormDirective, SACBootstrap5DropdownModule, SACBootstrap5LayoutModule],
@@ -19,6 +28,104 @@ describe('SacDropdownComponent', () => {
 
         cy.shouldHaveLabel('My Label');
         cy.get('select').should('exist');
-        cy.get('select').should('have.value', '0: null');
+        cy.get('select').find('option:selected').should('have.text', 'Please select');
+        cy.get('select').find('option:selected').should('have.value', '3: null');
+        cy.get('select').should('have.value', '3: null');
+    });
+
+    it('should preselect model value with option tags', () => {
+        cy.mount(
+            `<form>
+                <sac-dropdown 
+                    name="dropdown" 
+                    [label]="label"
+                    [emptyvalue]="null" 
+                    emptylabel="Please select"
+                    [(ngModel)]="value">
+                    <option [ngValue]="0">Value Item 1</option>
+                    <option [ngValue]="1">Value Item 2</option>
+                    <option [ngValue]="2">Value Item 3</option>
+                </sac-dropdown>
+            </form>`,
+            {
+                imports: [FormsModule, SacFormDirective, SACBootstrap5DropdownModule, SACBootstrap5LayoutModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: 1,
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.get('select').find('option:selected').should('have.text', 'Value Item 2');
+        cy.get('select').find('option:selected').should('have.value', '1: 1');
+    });
+
+    it('should preselect model value with options', () => {
+        cy.mount(
+            `<form>
+                <sac-dropdown 
+                    name="dropdown" 
+                    [label]="label"
+                    [emptyvalue]="null" 
+                    emptylabel="Please select"
+                    [options]="options"
+                    optionvalue="id"
+                    optionlabel="value"
+                    [(ngModel)]="value">
+                </sac-dropdown>
+            </form>`,
+            {
+                imports: [FormsModule, SacFormDirective, SACBootstrap5DropdownModule, SACBootstrap5LayoutModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: 1,
+                    options: [
+                        { id: 1, value: 'Element 1' },
+                        { id: 2, value: 'Element 2' },
+                        { id: 3, value: 'Element 3' },
+                    ],
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.get('select').find('option:selected').should('have.text', 'Element 1');
+        cy.get('select').find('option:selected').should('have.value', '1: 1');
+    });
+
+    it('should cannot select disabled element', () => {
+        cy.mount(
+            `<form>
+                <sac-dropdown 
+                    name="dropdown" 
+                    [label]="label"
+                    [emptyvalue]="null" 
+                    emptylabel="Please select"
+                    [options]="options"
+                    optionvalue="id"
+                    optionlabel="value"
+                    optionenabled="enabled"
+                    [(ngModel)]="value">
+                </sac-dropdown>
+            </form>`,
+            {
+                imports: [FormsModule, SacFormDirective, SACBootstrap5DropdownModule, SACBootstrap5LayoutModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: 1,
+                    options: [
+                        { id: 1, value: 'Element 1', enabled: true },
+                        { id: 2, value: 'Element 2', enabled: true },
+                        { id: 3, value: 'Element 3', enabled: false },
+                    ],
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.get('select').find('option:selected').should('have.text', 'Element 1');
+        cy.get('select').find('option:selected').should('have.value', '1: 1');
+        cy.get('select').find('option:disabled').should('have.text', 'Element 3');
     });
 });
--- ch.jnetwork.sac-controls/projects/sac-bootstrap5/src/controls/list/listbox.cy.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap5/src/controls/list/listbox.cy.ts
@@ -25,4 +25,92 @@ describe('SacListboxComponent', () => {
         cy.shouldHaveLabel('My Label');
         cy.get('select').should('exist');
     });
+
+    it('should preselect options with option tag', () => {
+        cy.mount(
+            `<form>
+                <sac-listbox name="listbox" [label]="label" [(ngModel)]="value">
+                    <option [ngValue]="1">Element 1</option>
+                    <option [ngValue]="2">Element 2</option>
+                    <option [ngValue]="3">Element 3</option>                
+                </sac-listbox>
+            </form>`,
+            {
+                imports: [FormsModule, SacFormDirective, SACBootstrap5ListModule, SACBootstrap5LayoutModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: [3],
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        // cy.get('select').select('Value 3');
+        cy.get('select').find('option:selected').should('have.value', 'Element 3');
+        cy.get('select').should('exist');
+    });
+
+    it('should preselect options with options', () => {
+        cy.mount(
+            `<form>
+                <sac-listbox 
+                    name="listbox"
+                    [label]="label"
+                    [(ngModel)]="value" 
+                    [options]="options"
+                    optionvalue="id"
+                    optionlabel="value">           
+                </sac-listbox>
+            </form>`,
+            {
+                imports: [FormsModule, SacFormDirective, SACBootstrap5ListModule, SACBootstrap5LayoutModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: [3],
+                    options: [
+                        { id: 1, value: 'Element 1' },
+                        { id: 2, value: 'Element 2' },
+                        { id: 3, value: 'Element 3' },
+                    ],
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        // cy.get('select').select('Value 3');
+        cy.get('select').find('option:selected').should('have.value', 'Element 3');
+        cy.get('select').should('exist');
+    });
+
+    it('should disable options', () => {
+        cy.mount(
+            `<form>
+                <sac-listbox 
+                    name="listbox"
+                    [label]="label"
+                    [(ngModel)]="value" 
+                    [options]="options"
+                    optionvalue="id"
+                    optionlabel="value"
+                    optionenabled="enabled">           
+                </sac-listbox>
+            </form>`,
+            {
+                imports: [FormsModule, SacFormDirective, SACBootstrap5ListModule, SACBootstrap5LayoutModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: [2],
+                    options: [
+                        { id: 1, value: 'Element 1', enabled: true },
+                        { id: 2, value: 'Element 2', enabled: true },
+                        { id: 3, value: 'Element 3', enabled: false },
+                    ],
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.get('select').find('option:selected').should('have.value', 'Element 2');
+        cy.get('select').find('option:disabled').should('have.text', 'Element 3');
+    });
 });

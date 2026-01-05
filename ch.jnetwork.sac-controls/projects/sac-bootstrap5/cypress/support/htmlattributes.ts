c3cfa9eae0c852e18093f0a00139789f03dc6c06
Philipp Jenni <philipp.jenni@exanic.ch>
2026-01-05 18:15:22 +0100

    feat: add tests to grid

--- ch.jnetwork.sac-controls/projects/sac-bootstrap5/cypress/support/htmlattributes.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap5/cypress/support/htmlattributes.ts
@@ -3,6 +3,7 @@ declare namespace Cypress {
         shouldBeReadonly(tagName?: string): Chainable;
         shouldBeDisabled(tagName?: string): Chainable;
         shouldHavePlaceholder(text: string, tagName?: string): Chainable;
+        isTruncated(): Chainable;
     }
 }
 
@@ -16,4 +17,10 @@ Cypress.Commands.add('shouldBeDisabled', (tagName: string = 'input') => {
 
 Cypress.Commands.add('shouldHavePlaceholder', (text: string, tagName: string = 'input') => {
     cy.get(tagName).should('have.attr', 'placeholder', text);
+});
+
+Cypress.Commands.add('isTruncated', { prevSubject: ['element'] }, (subject) => {
+    const el = subject[0];
+    expect(el.scrollWidth).to.be.greaterThan(el.offsetWidth);
+    return cy.wrap(subject);
 });

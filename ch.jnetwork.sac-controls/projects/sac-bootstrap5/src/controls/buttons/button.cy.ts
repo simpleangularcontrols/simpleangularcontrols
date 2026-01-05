5bac0c22c857b349acfafe8c559e90b873b74b00
Philipp Jenni <philipp.jenni@exanic.ch>
2026-01-05 15:23:17 +0100

    feat: complete tests for buttons

--- ch.jnetwork.sac-controls/projects/sac-bootstrap5/src/controls/buttons/button.cy.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap5/src/controls/buttons/button.cy.ts
@@ -153,14 +153,50 @@ describe('SacButtonComponent', () => {
     });
 
     it('should disable with string attribute', () => {
-        // TODO: test required
+        cy.mount(SacButtonComponent, {
+            componentProperties: {
+                name: 'button',
+                text: 'My Button',
+                isdisabled: 'true',
+                clicked: createOutputSpy('clickedSpy'),
+            },
+        });
+
+        cy.get('#button').should('have.text', 'My Button\n');
+        cy.get('#button').should('have.class', 'btn-secondary');
+        cy.get('#button').should('be.disabled');
+        cy.get('#button').click({ force: true });
+        cy.get('@clickedSpy').should('not.have.been.called');
     });
 
     it('should isloading with string attribute', () => {
-        // TODO: test required
+        cy.mount(SacButtonComponent, {
+            componentProperties: {
+                name: 'button',
+                text: 'My Button',
+                isloading: 'true',
+                clicked: createOutputSpy('clickedSpy'),
+            },
+        });
+
+        cy.get('#button').should('have.text', 'My Button\n');
+        cy.get('#button').should('have.class', 'btn-secondary');
+        cy.get('#button span.spinner-border').should('exist');
+        cy.get('#button').should('be.disabled');
+        cy.get('#button').click({ force: true });
+        cy.get('@clickedSpy').should('not.have.been.called');
     });
 
     it('should default style with empty role', () => {
-        // TODO: test required
+        cy.mount(SacButtonComponent, {
+            componentProperties: {
+                name: 'button',
+                text: 'My Button',
+                role: '',
+            },
+        });
+
+        cy.get('#button').should('have.text', 'My Button\n');
+        cy.get('#button').should('have.class', 'btn-secondary');
     });
 });

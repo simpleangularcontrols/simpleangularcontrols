f3129a5e34a2f9a9c509dde5403f18262d19ceb3
Philipp Jenni <philipp.jenni@exanic.ch>
2026-01-06 10:16:07 +0100

    feat: complete tests for formcontainer control

--- ch.jnetwork.sac-controls/projects/sac-bootstrap5/src/controls/static/formcontainer.cy.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap5/src/controls/static/formcontainer.cy.ts
@@ -22,8 +22,29 @@ describe('SacStaticFormContainerComponent', () => {
         cy.shouldHaveLabel('My Label');
         cy.get('sac-staticformcontainer').should('exist');
         cy.get('label').should('exist');
+        cy.get('label').should('not.have.class', 'required ');
         cy.get('input').should('exist');
     });
 
-    it('should have required class when required', () => {});
+    it('should have required class when required', () => {
+        cy.mount(
+            `<form>
+                <sac-staticformcontainer [label]="label" [isrequired]="true">
+                  <input  type="range" class="form-range" />
+                </sac-staticformcontainer>
+            </form>`,
+            {
+                imports: [FormsModule, SacFormDirective, SacStaticFormContainerComponent, SACBootstrap5LayoutModule],
+                componentProperties: {
+                    label: 'My Label',
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.get('sac-staticformcontainer').should('exist');
+        cy.get('label').should('exist');
+        cy.get('label').should('have.class', 'required');
+        cy.get('input').should('exist');
+    });
 });

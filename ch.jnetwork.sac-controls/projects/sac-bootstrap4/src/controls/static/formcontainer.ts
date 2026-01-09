15198f658d6bce10c431283d27dc0e4424ccad8c
Philipp Jenni <philipp.jenni@exanic.ch>
2026-01-09 17:55:52 +0100

    feat: add test for all static bs components

--- ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/static/formcontainer.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/static/formcontainer.ts
@@ -7,7 +7,7 @@ import { SacTooltipComponent } from '../tooltip/tooltip';
 import { NgClass, NgIf } from '@angular/common';
 import { Component, Host, Injector, Optional, forwardRef } from '@angular/core';
 import { NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
-import { SacStaticFormContainerCommon } from '@simpleangularcontrols/sac-common';
+import { SacStaticFormContainerCommon, SacTestingAttributePipe } from '@simpleangularcontrols/sac-common';
 
 /**
  * Component für einbindung eines beliebigen Controls in die Form Struktur
@@ -48,6 +48,7 @@ import { SacStaticFormContainerCommon } from '@simpleangularcontrols/sac-common'
         SacToControlWidthCssPipe,
         SacToLabelHeightPipe,
         SacTooltipComponent,
+        SacTestingAttributePipe,
     ],
 })
 export class SacStaticFormContainerComponent extends SacStaticFormContainerCommon {

a2bf8f26a94d7ebaacda6eb6daa2c185023a4887
Philipp Jenni <philipp.jenni@exanic.ch>
2026-01-09 17:56:03 +0100

    feat: add tests for all bs tabs

--- ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/tabs/tab.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/tabs/tab.ts
@@ -1,7 +1,7 @@
-import { Component, ContentChildren, QueryList } from '@angular/core';
-import { SacTabCommon } from '@simpleangularcontrols/sac-common';
 import { SacTabItemComponent } from './tabitem';
-import { NgFor, NgClass, NgStyle } from '@angular/common';
+import { NgClass, NgFor, NgStyle } from '@angular/common';
+import { Component, ContentChildren, QueryList } from '@angular/core';
+import { SacTabCommon, SacTestingAttributePipe } from '@simpleangularcontrols/sac-common';
 
 /**
  * Komponente für Tabs
@@ -10,24 +10,28 @@ import { NgFor, NgClass, NgStyle } from '@angular/common';
     selector: 'sac-tab',
     templateUrl: './tab.html',
     standalone: true,
-    imports: [
-        NgFor,
-        NgClass,
-        NgStyle,
-    ],
+    imports: [NgFor, NgClass, NgStyle, SacTestingAttributePipe],
 })
 export class SacTabComponent extends SacTabCommon {
-  /**
-   * Collection von TabItems
-   */
-  @ContentChildren(SacTabItemComponent)
-  _tabItems: QueryList<SacTabItemComponent>;
+    // #region Properties
 
-  /**
-   * Gibt die TabItems zurück
-   * @returns Array von TabItems
-   */
-  tabItems(): SacTabItemComponent[] {
-    return this._tabItems.toArray() as Array<SacTabItemComponent>;
-  }
+    /**
+     * Collection von TabItems
+     */
+    @ContentChildren(SacTabItemComponent)
+    public _tabItems: QueryList<SacTabItemComponent>;
+
+    // #endregion Properties
+
+    // #region Public Methods
+
+    /**
+     * Gibt die TabItems zurück
+     * @returns Array von TabItems
+     */
+    public tabItems(): SacTabItemComponent[] {
+        return this._tabItems.toArray() as Array<SacTabItemComponent>;
+    }
+
+    // #endregion Public Methods
 }

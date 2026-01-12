eee46f00aebec49368fa50b6122143f5a865254f
Philipp Jenni <philipp.jenni@exanic.ch>
2026-01-12 14:21:29 +0100

    feat: clone bs5 tests from wizard to all bs versions

---
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/wizard/wizard.cy.ts
@@ -0,0 +1,22 @@
+import { SacFormDirective } from '../form';
+import { SACBootstrap3LayoutModule } from '../layout/layout.module';
+import { SacWizardComponent } from './wizard';
+import { FormsModule } from '@angular/forms';
+
+describe('SacWizardComponent', () => {
+    it.skip('componen should exist', () => {
+        cy.mount(
+            `<form>
+                <sac-wizard [label]="label"></sac-wizard>
+            </form>`,
+            {
+                imports: [FormsModule, SacFormDirective, SacWizardComponent, SACBootstrap3LayoutModule],
+                componentProperties: {
+                    label: 'My Label',
+                },
+            }
+        );
+
+        cy.get('sac-wizard').should('exist');
+    });
+});

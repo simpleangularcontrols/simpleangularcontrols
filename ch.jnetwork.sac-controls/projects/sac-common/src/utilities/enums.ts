cf3c9e0f2c380700b4b6020f0fafe24b1667aa66
philippjenni <philipp.jenni@gmx.ch>
2026-03-15 15:50:14 +0100

    refactor: remive tslnt

--- ch.jnetwork.sac-controls/projects/sac-common/src/utilities/enums.ts
+++ ch.jnetwork.sac-controls/projects/sac-common/src/utilities/enums.ts
@@ -3,16 +3,16 @@
  */
 export enum TooltipPosition {
     none = 0,
-    // tslint:disable-next-line:no-bitwise
+    // eslint-disable-next-line no-bitwise
     top = 1 << 0,
-    // tslint:disable-next-line:no-bitwise
+    // eslint-disable-next-line no-bitwise
     right = 1 << 1,
-    // tslint:disable-next-line:no-bitwise
+    // eslint-disable-next-line no-bitwise
     bottom = 1 << 2,
-    // tslint:disable-next-line:no-bitwise
+    // eslint-disable-next-line no-bitwise
     left = 1 << 3,
-    // tslint:disable-next-line:no-bitwise
+    // eslint-disable-next-line no-bitwise
     topend = 1 << 4,
-    // tslint:disable-next-line:no-bitwise
+    // eslint-disable-next-line no-bitwise
     bottomend = 1 << 5,
 }

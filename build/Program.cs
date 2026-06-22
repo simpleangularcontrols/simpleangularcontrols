7c5b2bcaede80367210d5be987b244d4558b4e69
Philipp Jenni <philipp.jenni@gmx.ch>
2026-06-22 18:12:54 +0200

    fix: Security Issue in Nuget.Command

--- build/Program.cs
+++ build/Program.cs
@@ -17,7 +17,7 @@ public static class Program
         return new CakeHost()
             .UseContext<BuildContext>()
             .UseLifetime<Lifetime>()
-            .InstallTool(new Uri("nuget:?package=NuGet.CommandLine&version=5.11.0"))
+            .InstallTool(new Uri("nuget:?package=NuGet.CommandLine&version=5.11.7"))
             .InstallTool(new Uri("dotnet:?package=dotnet-reportgenerator-globaltool&version=5.5.1"))
             .InstallTool(new Uri("nuget:?package=CodecovUploader&version=0.8.0"))
             .Run(args);

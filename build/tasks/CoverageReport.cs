d63cfbe6d43914614655d18713add51c4b6ae1a7
Philipp Jenni <philipp.jenni@exanic.ch>
2026-06-25 12:09:00 +0200

    feat: Add CodeCov

--- build/tasks/CoverageReport.cs
+++ build/tasks/CoverageReport.cs
@@ -1,9 +1,13 @@
 ﻿using Build.context;
 using Build.extensions;
+using Cake.Codecov;
+using Cake.Common;
 using Cake.Common.Tools.ReportGenerator;
+using Cake.Core.Diagnostics;
 using Cake.Core.IO;
 using Cake.Frosting;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Build.tasks
 {
@@ -20,6 +24,7 @@ namespace Build.tasks
             ConvertCoverageReport(context, "bootstrap3");
             ConvertCoverageReport(context, "bootstrap4");
             ConvertCoverageReport(context, "bootstrap5");
+            CreateCodeCovReport(context, "bootstrap3", "bootstrap4", "bootstrap5");
         }
 
         public override bool ShouldRun(BuildContext context)
@@ -48,6 +53,42 @@ namespace Build.tasks
             context.ReportGenerator(coverageReports, context.Environment.WorkingDirectory.Combine(context.DirectoryProject.ToDirectoryPath())
                                                                                          .Combine("reports")
                                                                                          .Combine($"coverage-{bootstrapVersion}"), setting);
+        }
+
+        private static void CreateCodeCovReport(BuildContext context, params string[] bootstrapVersions)
+        {
+            if (string.IsNullOrEmpty(context.EnvironmentVariable<string>("CODECOV_TOKEN", string.Empty)))
+            {
+                context.Log.Warning("CODECOV_TOKEN environment variable is not set. Skipping Codecov upload.");
+                return;
+            }
+
+            List<FilePath> coverageReports = bootstrapVersions.Select(version => context.Environment.WorkingDirectory.Combine(context.DirectoryProject.ToDirectoryPath())
+                                                                                                                     .Combine("coverage")
+                                                                                                                     .Combine(version)
+                                                                                                                     .CombineWithFilePath("lcov.info")
+                                                                                                                     .Collapse()).ToList();
+
+            if (!coverageReports.Any())
+            {
+                context.Log.Warning("No lcov.info reports were found for this upload.");
+                return;
+            }
+
+            context.Log.Information($"Uploading {coverageReports.Count()} reports to Codecov...");
+
+            var settings = new CodecovSettings
+            {
+                WorkingDirectory = context.Environment.WorkingDirectory.Combine(context.DirectoryProject.ToDirectoryPath())
+                                                                       .Combine("projects")
+                                                                       .Collapse(),
+                Files = coverageReports.Select(x => x.FullPath),
+                Token = context.EnvironmentVariable<string>("CODECOV_TOKEN", string.Empty),
+                Slug = "simpleangularcontrols/simpleangularcontrols",
+                Flags = "angular-16"
+            };
+
+            context.Codecov(settings);
         }
     }
 }

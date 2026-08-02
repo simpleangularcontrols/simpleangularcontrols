2838e65d9637469066950264ce43d2caf4459755
philippjenni <philipp.jenni@gmx.ch>
2026-08-02 13:05:18 +0200

    feat: add tests for validation error position

--- ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/input/input.cy.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/input/input.cy.ts
@@ -53,6 +53,224 @@ describe('NgInputComponent', () => {
         cy.get('input').should('have.value', 'My Text');
     });
 
+    it('should show required', () => {
+        cy.mount(
+            `<form>
+                <sac-input name="field" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
+                </sac-input>
+            </form>`,
+            {
+                declarations: [SacFormDirective, SacInputComponent],
+                imports: [FormsModule, SACBootstrap3LayoutModule, SACCommonUtliltiesModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: '',
+                    valueChange: createOutputSpy('valueSpy'),
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.shouldBeInvalid();
+
+        cy.get('input').should('have.value', '');
+        cy.get('input').type('My Text');
+
+        cy.shouldBeValid();
+
+        cy.get('input').should('have.value', 'My Text');
+    });
+
+    it('should show required with inline error in control', () => {
+        cy.mount(
+            `<form>
+                <sac-input name="field" [inlineerrorenabled]="true" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
+                </sac-input>
+            </form>`,
+            {
+                declarations: [SacFormDirective, SacInputComponent],
+                imports: [FormsModule, SACBootstrap3LayoutModule, SACCommonUtliltiesModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: '',
+                    valueChange: createOutputSpy('valueSpy'),
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.shouldBeInvalid();
+        cy.shouldHaveInlineErrorMessage();
+
+        cy.get('input').should('have.value', '');
+        cy.get('input').type('My Text');
+
+        cy.shouldBeValid();
+
+        cy.get('input').should('have.value', 'My Text');
+    });
+
+    it('should show required without inline error in control', () => {
+        cy.mount(
+            `<form>
+                <sac-input name="field" [inlineerrorenabled]="false" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
+                </sac-input>
+            </form>`,
+            {
+                declarations: [SacFormDirective, SacInputComponent],
+                imports: [FormsModule, SACBootstrap3LayoutModule, SACCommonUtliltiesModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: '',
+                    valueChange: createOutputSpy('valueSpy'),
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.shouldBeInvalid();
+        cy.shouldNotHaveInlineErrorMessage();
+
+        cy.get('input').should('have.value', '');
+        cy.get('input').type('My Text');
+
+        cy.shouldBeValid();
+
+        cy.get('input').should('have.value', 'My Text');
+    });
+
+    it('should show required with inline error in formlayout', () => {
+        cy.mount(
+            `<form [sacFormLayout] [inlineError]="true">
+                <sac-input name="field" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
+                </sac-input>
+            </form>`,
+            {
+                declarations: [SacFormDirective, SacInputComponent],
+                imports: [FormsModule, SACBootstrap3LayoutModule, SACCommonUtliltiesModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: '',
+                    valueChange: createOutputSpy('valueSpy'),
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.shouldBeInvalid();
+        cy.shouldHaveInlineErrorMessage();
+
+        cy.get('input').should('have.value', '');
+        cy.get('input').type('My Text');
+
+        cy.shouldBeValid();
+
+        cy.get('input').should('have.value', 'My Text');
+    });
+
+    it('should show required without inline error in formlayout', () => {
+        cy.mount(
+            `<form [sacFormLayout] [inlineError]="false">
+                <sac-input name="field" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
+                </sac-input>
+            </form>`,
+            {
+                declarations: [SacFormDirective, SacInputComponent],
+                imports: [FormsModule, SACBootstrap3LayoutModule, SACCommonUtliltiesModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: '',
+                    valueChange: createOutputSpy('valueSpy'),
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.shouldBeInvalid();
+        cy.shouldNotHaveInlineErrorMessage();
+
+        cy.get('input').should('have.value', '');
+        cy.get('input').type('My Text');
+
+        cy.shouldBeValid();
+
+        cy.get('input').should('have.value', 'My Text');
+    });
+
+    it('should show required with inline error in configuration service', () => {
+        cy.mount(
+            `<form>
+                <sac-input name="field" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
+                </sac-input>
+            </form>`,
+            {
+                declarations: [SacFormDirective, SacInputComponent],
+                imports: [FormsModule, SACBootstrap3LayoutModule, SACCommonUtliltiesModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: '',
+                    valueChange: createOutputSpy('valueSpy'),
+                },
+                providers: [
+                    {
+                        provide: SACCONFIGURATION_SERVICE,
+                        useValue: {
+                            InlineErrorEnabled: true,
+                        },
+                    },
+                ],
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.shouldBeInvalid();
+        cy.shouldHaveInlineErrorMessage();
+
+        cy.get('input').should('have.value', '');
+        cy.get('input').type('My Text');
+
+        cy.shouldBeValid();
+
+        cy.get('input').should('have.value', 'My Text');
+    });
+
+    it('should show required without inline error in configuration service', () => {
+        cy.mount(
+            `<form>
+                <sac-input name="field" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
+                </sac-input>
+            </form>`,
+            {
+                declarations: [SacFormDirective, SacInputComponent],
+                imports: [FormsModule, SACBootstrap3LayoutModule, SACCommonUtliltiesModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: '',
+                    valueChange: createOutputSpy('valueSpy'),
+                },
+                providers: [
+                    {
+                        provide: SACCONFIGURATION_SERVICE,
+                        useValue: {
+                            InlineErrorEnabled: false,
+                        },
+                    },
+                ],
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.shouldBeInvalid();
+        cy.shouldNotHaveInlineErrorMessage();
+
+        cy.get('input').should('have.value', '');
+        cy.get('input').type('My Text');
+
+        cy.shouldBeValid();
+
+        cy.get('input').should('have.value', 'My Text');
+    });
+
     it('should hide label', () => {
         cy.mount(
             `<form>

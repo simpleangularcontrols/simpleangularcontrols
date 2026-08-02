d35d02f232ea81c4c71615c828d45569aa312390
philippjenni <philipp.jenni@gmx.ch>
2026-08-02 19:48:47 +0200

    feat: add tests for tooltip right alignment with helptext

--- ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/input/input.cy.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap4/src/controls/input/input.cy.ts
@@ -74,14 +74,14 @@ describe('NgInputComponent', () => {
         cy.get('label sac-tooltip').should('exist');
     });
 
-    it('should show helptext as text by control', () => {
+    it('should show helptext as tooltip with splitview by control', () => {
         cy.mount(
             `<form>
-                <sac-input name="field" helptextmode="text" helptext="This is a Helptext" [label]="label" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
+                <sac-input name="field" [splitlabelandhelptext]="true" helptextmode="tooltip" helptext="This is a Helptext" [label]="label" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
                 </sac-input>
               </form>`,
             {
-                declarations: [SacFormDirective, SacInputComponent],
+                declarations: [SacFormDirective, SacInputComponent, SacTooltipComponent],
                 imports: [FormsModule, SACBootstrap4LayoutModule, SACCommonUtliltiesModule],
                 componentProperties: {
                     label: 'My Label',
@@ -93,6 +93,65 @@ describe('NgInputComponent', () => {
 
         cy.shouldHaveLabel('My Label');
         cy.get('input').should('have.value', 'My Text');
+        cy.get('.help-block').should('not.exist');
+        cy.get('label sac-tooltip').should('exist');
+        cy.get('label .flex-sm-grow-1').should('exist');
+    });
+
+    it('should show helptext as tooltip with splitview by formcontrol', () => {
+        cy.mount(
+            `<form sacFormLayout [splitlabelandhelptext]="true">
+                <sac-input name="field" helptextmode="tooltip" helptext="This is a Helptext" [label]="label" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
+                </sac-input>
+              </form>`,
+            {
+                declarations: [SacFormDirective, SacInputComponent, SacTooltipComponent],
+                imports: [FormsModule, SACBootstrap4LayoutModule, SACCommonUtliltiesModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: 'My Text',
+                    valueChange: createOutputSpy('valueSpy'),
+                },
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.get('input').should('have.value', 'My Text');
+        cy.get('.help-block').should('not.exist');
+        cy.get('label sac-tooltip').should('exist');
+        cy.get('label .flex-sm-grow-1').should('exist');
+    });
+
+    it('should show helptext as tooltip with splitview by configuration service', () => {
+        cy.mount(
+            `<form>
+                <sac-input name="field" helptextmode="tooltip" helptext="This is a Helptext" [label]="label" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
+                </sac-input>
+              </form>`,
+            {
+                declarations: [SacFormDirective, SacInputComponent, SacTooltipComponent],
+                imports: [FormsModule, SACBootstrap4LayoutModule, SACCommonUtliltiesModule],
+                componentProperties: {
+                    label: 'My Label',
+                    value: 'My Text',
+                    valueChange: createOutputSpy('valueSpy'),
+                },
+                providers: [
+                    {
+                        provide: SACCONFIGURATION_SERVICE,
+                        useValue: {
+                            SplitLabelAndHelptext: true,
+                        },
+                    },
+                ],
+            }
+        );
+
+        cy.shouldHaveLabel('My Label');
+        cy.get('input').should('have.value', 'My Text');
+        cy.get('.help-block').should('not.exist');
+        cy.get('label sac-tooltip').should('exist');
+        cy.get('label .flex-sm-grow-1').should('exist');
     });
 
     it('should show label and text with large height by control', () => {
@@ -273,7 +332,7 @@ describe('NgInputComponent', () => {
         cy.get('input').should('have.value', 'My Text');
     });
 
-    it.only('should show required with inline error in control', () => {
+    it('should show required with inline error in control', () => {
         cy.mount(
             `<form>
                 <sac-input name="field" [inlineerrorenabled]="true" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
@@ -302,7 +361,7 @@ describe('NgInputComponent', () => {
         cy.get('input').should('have.value', 'My Text');
     });
 
-    it.only('should show required without inline error in control', () => {
+    it('should show required without inline error in control', () => {
         cy.mount(
             `<form>
                 <sac-input name="field" [inlineerrorenabled]="false" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
@@ -331,7 +390,7 @@ describe('NgInputComponent', () => {
         cy.get('input').should('have.value', 'My Text');
     });
 
-    it.only('should show required with inline error in formlayout', () => {
+    it('should show required with inline error in formlayout', () => {
         cy.mount(
             `<form [sacFormLayout] [inlineError]="true">
                 <sac-input name="field" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
@@ -360,7 +419,7 @@ describe('NgInputComponent', () => {
         cy.get('input').should('have.value', 'My Text');
     });
 
-    it.only('should show required without inline error in formlayout', () => {
+    it('should show required without inline error in formlayout', () => {
         cy.mount(
             `<form [sacFormLayout] [inlineError]="false">
                 <sac-input name="field" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
@@ -389,7 +448,7 @@ describe('NgInputComponent', () => {
         cy.get('input').should('have.value', 'My Text');
     });
 
-    it.only('should show required with inline error in configuration service', () => {
+    it('should show required with inline error in configuration service', () => {
         cy.mount(
             `<form>
                 <sac-input name="field" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
@@ -426,7 +485,7 @@ describe('NgInputComponent', () => {
         cy.get('input').should('have.value', 'My Text');
     });
 
-    it.only('should show required without inline error in configuration service', () => {
+    it('should show required without inline error in configuration service', () => {
         cy.mount(
             `<form>
                 <sac-input name="field" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">
@@ -463,7 +522,7 @@ describe('NgInputComponent', () => {
         cy.get('input').should('have.value', 'My Text');
     });
 
-    it.only('should show required with inline error by default', () => {
+    it('should show required with inline error by default', () => {
         cy.mount(
             `<form>
                 <sac-input name="field" [label]="label" [isrequired]="true" [ngModel]="value" (ngModelChange)="valueChange.emit($event)">

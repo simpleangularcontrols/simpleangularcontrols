1763a02a3da20d3a884b58930bd1611121a04d37
philippjenni <philipp.jenni@gmx.ch>
2026-08-04 23:02:45 +0200

    fix: extension validation tests has invalid markup

--- ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/upload/upload.cy.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap3/src/controls/upload/upload.cy.ts
@@ -152,7 +152,7 @@ describe('SacUploadComponent', () => {
         cy.registerUploadController(filesize).then((_) => {
             cy.mount(
                 `<form>
-                    <sac-upload name="uploadControl" allowedtypes=".txt|.csv" (onfileerror)="fileerrorAction.emit($event)" endpoint="/api/upload/register" [label]="label"></sac-upload>
+                    <sac-upload name="uploadControl" allowedtypes=".txt,.csv" (onfileerror)="fileerrorAction.emit($event)" endpoint="/api/upload/register" [label]="label"></sac-upload>
                 </form>`,
                 {
                     imports: [FormsModule, SacFormDirective, SACBootstrap3UploadModule, SACBootstrap3LayoutModule],
--- ch.jnetwork.sac-controls/projects/sac-bootstrap5/src/controls/upload/dropzonemultiple.cy.ts
+++ ch.jnetwork.sac-controls/projects/sac-bootstrap5/src/controls/upload/dropzonemultiple.cy.ts
@@ -92,7 +92,7 @@ describe('SacDropzoneMultipleComponent', () => {
         cy.registerUploadController(filesize).then((_) => {
             cy.mount(
                 `<form>
-                    <sac-dropzonemultiple name="uploadControl" allowedtypes=".txt|.csv" (onfileerror)="fileerrorAction.emit($event)" endpoint="/api/upload/register" [label]="label"></sac-dropzonemultiple>
+                    <sac-dropzonemultiple name="uploadControl" allowedtypes=".txt,.csv" (onfileerror)="fileerrorAction.emit($event)" endpoint="/api/upload/register" [label]="label"></sac-dropzonemultiple>
                 </form>`,
                 {
                     imports: [FormsModule, SacFormDirective, SACBootstrap5UploadModule, SACBootstrap5LayoutModule],
